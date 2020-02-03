5a768dd31e152366a007393cc8d7d5f84faeb76a
Allen Chen <allenmuhanchen@gmail.com>
2020-02-03 12:40:00 -0500

    Continued renaming

--- xper-train/xper-allen/app/org/xper/allen/app/blockGenerators/trials/trial.java
+++ xper-train/xper-allen/app/org/xper/allen/app/blockGenerators/trials/trial.java
@@ -3,6 +3,6 @@ package org.xper.allen.app.blockGenerators.trials;
 public interface trial {
 	public void setStimObjData();
 	public int[] getStimObjData(int[] stimObjData);
-	public void setEStimSpecs();
-	public int[] getEStimSpecs(int[] eStimSpecs);
+	public void setEStimObjData();
+	public int[] getEStimObjData(int[] eStimObjData);
 }
