d2d90596807b66db8961f703a46814c21997f104
Allen Chen <allenmuhanchen@gmail.com>
2020-03-02 17:58:45 -0500

    TrainingGenerator args
    TrainingGenerator can now take two arguments:
    1. blockId
    2. visualTypes: comma separated list of stimObjIDs

--- xper-train/xper-allen/app/org/xper/allen/app/blockGenerators/TrainingGenerator.java
+++ xper-train/xper-allen/app/org/xper/allen/app/blockGenerators/TrainingGenerator.java
@@ -1,17 +1,34 @@
 package org.xper.allen.app.blockGenerators;
 
+import java.util.ArrayList;
+
 import org.springframework.config.java.context.JavaConfigApplicationContext;
 import org.xper.util.FileUtil;
 
 public class TrainingGenerator {
-	static int blockId = 5;
 	public static void main(String[] args) {
-
 		JavaConfigApplicationContext context = new JavaConfigApplicationContext(
 				FileUtil.loadConfigClass("experiment.ga.config_class"));
 
 		trainingBlockGen gen = context.getBean(trainingBlockGen.class);
-		gen.generate(blockId);
+		
+		try {
+			//blockId
+			int blockId = Integer.parseInt(args[0]);
+			//visTypes
+			String csvInput = args[1];
+			String[] elements = csvInput.split(",");
+			ArrayList<Integer> visualTypes = new ArrayList<Integer>();
+			for (String s:elements) {
+				visualTypes.add(Integer.parseInt(s));
+			}
+			gen.generate(blockId, visualTypes);
+		}
+		catch(Exception e) {
+			System.out.println("Not enough arguments were given. args[0]: int blockId, args[1]: visualTypes (comma separated list of StimObjIDs) ");
+		
+		
 
+		}
 	}
 }
