5be8d7498bfc6484a7e7a4f8b1eddb92267772fa
Allen Chen <allenmuhanchen@gmail.com>
2022-01-21 14:38:25 -0500

    cleanup git repo

--- shellScripts/cTrain_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp /Users/allenchen/Documents/GitHub/V1Microstim/shellScripts/xper.properties.allen.properties /Users/allenchen/Documents/GitHub/V1Microstim/xper-train/dist/allen/xper.properties
-cd /Users/allenchen/Documents/GitHub/V1Microstim/xper-train/dist/allen
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=localhost:5005 -jar console.jar
--- shellScripts/trainGen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp /Users/allenchen/Documents/GitHub/V1Microstim/shellScripts/xper.properties.allen.properties /Users/allenchen/Documents/GitHub/V1Microstim/xper-train/dist/allen/xper.properties
-cd /Users/allenchen/Documents/GitHub/V1Microstim/xper-train/dist/allen
-java -jar trainingGeneration.jar '/Users/allenchen/Documents/GitHub/V1Microstim/xper-train/xper-allen/doc/Test.xml'
