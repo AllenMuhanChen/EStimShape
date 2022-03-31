4ee97e492b3285520f3dd0f63e3e8acd3a6af864
Allen Chen <allenmuhanchen@gmail.com>
2022-03-31 13:17:06 -0400

    small updates

--- xper-train/xper-allen/src/org/xper/allen/eye/headfree/HeadFreeIscanDevice.java
+++ xper-train/xper-allen/src/org/xper/allen/eye/headfree/HeadFreeIscanDevice.java
@@ -2,8 +2,10 @@ package org.xper.allen.eye.headfree;
 
 import org.xper.Dependency;
 import org.xper.acq.device.AcqSamplingDevice;
+import org.xper.drawing.Coordinates2D;
 import org.xper.eye.IscanDevice;
 import org.xper.eye.listener.EyeDeviceMessageListener;
+import org.xper.eye.zero.EyeZeroMessageListener;
 
 public class HeadFreeIscanDevice extends IscanDevice implements HeadFreeEyeZeroAdjustable{
 	@Dependency
@@ -27,10 +29,25 @@ public class HeadFreeIscanDevice extends IscanDevice implements HeadFreeEyeZeroA
 			if (isIn(getEyeZeroAlgorithm().getEyeZeroUpdateEyeWinCenter(), getEyeZeroAlgorithm().getEyeZeroUpdateEyeWinThreshold())) {
 				getEyeZeroAlgorithm().collectEyeZeroSignal(getVoltage());
 			}
+			if(isIn(getEyeZeroAlgorithm().getEyeZeroUpdateEyeWinCenter(), getEyeZeroAlgorithm().getEyeZeroInnerThreshold())) {
+				getEyeZeroAlgorithm().collectEyeZeroInnerSignal(getVoltage());
+			}
 		}
-
 	}
+	
+	public void calculateNewEyeZero() {
+		Coordinates2D z = getEyeZeroAlgorithm().getNewEyeZero();
 
+		if (z != null) {
+			setEyeZero(z);	
+			System.out.println("AC8484932: newEyeZero = "+z.getX() + ", "+z.getY());
+		}
+		if (getEyeZeroMessageListener() != null) {
+			for (EyeZeroMessageListener listener : getEyeZeroMessageListener()) {
+				listener.eyeZeroMessage(getLocalTimeUtil().currentTimeMicros(), getId(), getEyeZero());
+			}
+		}
+	}
 
 
 	public void startEyeZeroInnerSignalCollection() {
