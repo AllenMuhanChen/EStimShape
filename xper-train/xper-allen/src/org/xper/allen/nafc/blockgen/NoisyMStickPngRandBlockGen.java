88afeba6cb15af730b9a4b3e1664d4710e279648
Allen Chen <allenmuhanchen@gmail.com>
2022-05-24 14:34:22 -0400

    Attempt 1 at cleaner blockgen

--- xper-train/xper-allen/src/org/xper/allen/nafc/blockgen/NoisyMStickPngRandBlockGen.java
+++ xper-train/xper-allen/src/org/xper/allen/nafc/blockgen/NoisyMStickPngRandBlockGen.java
@@ -218,7 +218,8 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 		} catch (VariableNotFoundException e) {
 			dbUtil.writeReadyGenerationInfo(genId, 0);
 		}
-
+		List<List<QualitativeMorphParams>> qmps = new LinkedList<>();
+		List<MetricMorphParams> mmps = new LinkedList<>();
 		int nSuccess = 0;
 		for (int i = 0; i < numTrials; i++) {
 			int numQMDistractors = numQMDistractorsTrialList.get(i);
@@ -236,11 +237,12 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 
 
 			//SETTING MORPHS - we never want to change our morph because of a fail. Otherwise probability distribution of morph types will be skewed.
-			MetricMorphParams mmp = mmpGenerator.getMMP(sampleScaleUpperLim, numMMCategories);
-
-			List<QualitativeMorphParams> qmps = new LinkedList<>();
+			MetricMorphParams mmp = new MetricMorphParams();
+			mmp = mmpGenerator.getMMP(sampleScaleUpperLim, numMMCategories);
+			mmps.add(mmp);
+			
 			for(int qmIndx = 0; qmIndx<numQMDistractors; qmIndx++) {
-				qmps.add(qmpGenerator.getQMP(numQMCategories));
+				qmps.get(i).add(qmpGenerator.getQMP(numQMCategories));
 			}
 
 			while (tryagain){
@@ -328,7 +330,7 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 						while(nTries_qm < maxAttempts_qm) {
 							try {
 								setProperties(objs_distractor.get(i).get(j));
-								qmDistractorSuccess[j] = objs_distractor.get(i).get(j).genQualitativeMorphedLeafMatchStick(leafToMorphIndx, objs_sample.get(i), qmps.get(j));
+								qmDistractorSuccess[j] = objs_distractor.get(i).get(j).genQualitativeMorphedLeafMatchStick(leafToMorphIndx, objs_sample.get(i), qmps.get(i).get(j));
 							} catch (Exception e) {
 								e.printStackTrace();
 								qmDistractorSuccess[j]=false;
@@ -357,7 +359,9 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 				}
 
 			}
+		
 
+		
 			//GENERATING RAND DISTRACTORS
 			System.out.println("Trying to Generate Rand Distractor");
 			boolean randDistractorsSuccess = false;
@@ -382,59 +386,77 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 				tryagain = false;
 			}
 
-
-			//GENERATING STIM-OBJ SPECS & WRITE TO DB
-			//Determine Ids
-			long sampleId = globalTimeUtil.currentTimeMicros();
-			long matchId = sampleId + 1;
-			List<Long> distractorIds = new LinkedList<Long>();
-			for (int j=0; j<objs_distractor.get(i).size(); j++){
-				distractorIds.add(matchId + j + 1);
-			}
-
-			//GENERATE PNGS
-			List<AllenMatchStick> objs = new LinkedList<AllenMatchStick>();
-			List<AllenMatchStick> objs_noise = new LinkedList<AllenMatchStick>();
-			List<List<String>> labels = new LinkedList<>();
-			List<List<String>> noiseLabels = new LinkedList<>();
-			//SAMPLE
+		}
+		
+		List<AllenMatchStick> objs = new LinkedList<AllenMatchStick>();
+		List<AllenMatchStick> objs_noise = new LinkedList<AllenMatchStick>();
+		List<List<String>> labels = new LinkedList<>();
+		List<List<String>> noiseLabels = new LinkedList<>();
+		List<Long> ids = new LinkedList<Long>();
+		List<Long> ids_noise = new LinkedList<Long>();
+		List<NoiseData> noiseData= new LinkedList<NoiseData>();
+		List<Long> sampleIds = new LinkedList<Long>();
+		List<Long> matchIds = new LinkedList<Long>();
+		List<List<Long>> distractorIds = new LinkedList<>();
+		for (int i = 0; i < numTrials; i++) {
+			int numQMDistractors = numQMDistractorsTrialList.get(i);
+			int numRandDistractors = numDistractorsTrialList.get(i)-numQMDistractors;
+			//add objs for pngs to draw
 			objs.add(objs_sample.get(i)); 
 			objs_noise.add(objs_sample.get(i));
+			objs.add(objs_match.get(i));
+			objs.addAll(objs_distractor.get(i));
+			
+			//Determine Ids
+			long sampleId = globalTimeUtil.currentTimeMicros();
+			sampleIds.add(sampleId);
+			long matchId = sampleId + 1;
+			matchIds.add(matchId);
+			List<Long> distractorId = new LinkedList<Long>();
+			for (int j=0; j<objs_distractor.get(i).size(); j++){
+				distractorId.add(matchId + j + 1);
+			}
+			distractorIds.add(distractorId);
+			
+			ids.add(sampleId);
+			ids_noise.add(sampleId);
+			ids.add(matchId);
+			ids.addAll(distractorId);
+			
+			
+			//LABELS
 			List<String> sampleLabels = Arrays.asList(new String[] {"sample"});
 			labels.add(sampleLabels);
 			noiseLabels.add(sampleLabels);
-			//MATCH
-			objs.add(objs_match.get(i));
 			List<String> matchLabels = Arrays.asList(new String[] {"match"});
 			labels.add(matchLabels);
-			//DISTRACTORS
-			objs.addAll(objs_distractor.get(i));
 			for(int k=0; k<numQMDistractors; k++) {
 				List<String> distractorLabels = Arrays.asList(new String[] {"distractor", "qualitativeMorph"});
 				labels.add(distractorLabels);
 			}
+
 			for(int k=0; k<numRandDistractors; k++) {
 				List<String> distractorLabels = Arrays.asList(new String[] {"distractor", "rand"});
 				labels.add(distractorLabels);
 			}
+			
+			//SETTING NOISE DATA
+			NoiseType noiseType = noiseTypesTrialList.get(i);
+			double[] noiseChance = noiseChancesTrialList.get(i);
+			noiseData.add(objs_noise.get(i).setNoiseParameters(noiseType, noiseChance));
+		}
+		
+		
+		List<String> stimPaths = pngMaker.createAndSavePNGsfromObjs(objs, ids, labels);
+		stimPaths = convertPathsToExperiment(stimPaths);
+		List<String> noiseMapPaths = pngMaker.createAndSaveNoiseMapfromObjs(objs_noise, ids_noise, noiseLabels);
+		noiseMapPaths = convertPathsToExperiment(noiseMapPaths);
+		for (int i = 0; i < numTrials; i++) {
+			int numQMDistractors = numQMDistractorsTrialList.get(i);
+			int numRandDistractors = numDistractorsTrialList.get(i)-numQMDistractors;
+			if(numRandDistractors<0) throw new IllegalArgumentException("There should not be less than 0 randDistractors");
+			int numQMCategories = numCategoriesMorphedTrialList.get(i);
 
-			List<Long> ids = new LinkedList<Long>();
-			List<Long> ids_noise = new LinkedList<Long>();
-			ids.add(sampleId);
-			ids_noise.add(sampleId);
-			ids.add(matchId);
-			ids.addAll(distractorIds);
-			List<String> stimPaths = pngMaker.createAndSavePNGsfromObjs(objs, ids, labels);
-			stimPaths = convertPathsToExperiment(stimPaths);
-
-			//NOISE MAP
-			NoiseData noiseData = objs_noise.get(0).setNoiseParameters(noiseType, noiseChance);
-			List<String> noiseMapPaths = new ArrayList<String>();
-			noiseMapPaths.add("");
-			if(noiseType!=NoiseType.NONE) {
-				noiseMapPaths = pngMaker.createAndSaveNoiseMapfromObjs(objs_noise, ids_noise, noiseLabels);
-			} 
-			noiseMapPaths = convertPathsToExperiment(noiseMapPaths);
 
 			//SAVE SPECS.TXT
 			for(int k=0; k<objs.size(); k++) {
@@ -451,7 +473,7 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 			Coordinates2D matchCoords = ddUtil.getMatchCoords();
 
 			//SAMPLE SPEC
-			long taskId = sampleId;
+			long taskId = sampleIds.get(i);
 			NoisyPngSpec sampleSpec = new NoisyPngSpec();
 			//			sampleSpec.setPath(experimentPngPath+"/"+ids.get(0)+".png");
 			sampleSpec.setPath(stimPaths.get(0));
@@ -460,7 +482,7 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 			sampleSpec.setyCenter(sampleCoords.getY());
 			ImageDimensions sampleDimensions = new ImageDimensions(sampleScaleUpperLim, sampleScaleUpperLim);
 			sampleSpec.setImageDimensions(sampleDimensions);
-			dbUtil.writeStimObjData(sampleId, sampleSpec.toXml(), "Sample");
+			dbUtil.writeStimObjData(sampleIds.get(i), sampleSpec.toXml(), "Sample");
 
 			//
 			long[] choiceIds = new long[numChoices];
@@ -473,8 +495,8 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 			matchSpec.setyCenter(matchCoords.getY());
 			ImageDimensions matchDimensions = new ImageDimensions(sampleScaleUpperLim, sampleScaleUpperLim);
 			matchSpec.setImageDimensions(matchDimensions);
-			dbUtil.writeStimObjData(matchId, matchSpec.toXml(), "Match");
-			choiceIds[0] = matchId;
+			dbUtil.writeStimObjData(matchIds.get(i), matchSpec.toXml(), "Match");
+			choiceIds[0] = matchIds.get(i);
 
 			//DISTRACTORS SPECS
 			List<NoisyPngSpec> distractorSpec = new ArrayList<NoisyPngSpec>();
@@ -487,8 +509,8 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 				ImageDimensions distractorDimensions = new ImageDimensions(distractorScaleUpperLim, distractorScaleUpperLim);
 				distractorSpec.get(j).setImageDimensions(distractorDimensions);
 
-				dbUtil.writeStimObjData(distractorIds.get(j), distractorSpec.get(j).toXml(), "QM");
-				choiceIds[j+1] = distractorIds.get(j);
+				dbUtil.writeStimObjData(distractorIds.get(i).get(j), distractorSpec.get(j).toXml(), "QM");
+				choiceIds[j+1] = distractorIds.get(i).get(j);
 			}
 			for(int j=numQMDistractors; j<numChoices-1; j++){
 				distractorSpec.add(j, new NoisyPngSpec());
@@ -498,8 +520,8 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 				distractorSpec.get(j).setyCenter(distractorsCoords.get(j).getY());
 				ImageDimensions distractorDimensions = new ImageDimensions(distractorScaleUpperLim, distractorScaleUpperLim);
 				distractorSpec.get(j).setImageDimensions(distractorDimensions);
-				dbUtil.writeStimObjData(distractorIds.get(j), distractorSpec.get(j).toXml(), "RAND");
-				choiceIds[j+1] = distractorIds.get(j);
+				dbUtil.writeStimObjData(distractorIds.get(i).get(j), distractorSpec.get(j).toXml(), "RAND");
+				choiceIds[j+1] = distractorIds.get(i).get(j);
 			}
 
 			//PREPARING WRITING STIM-SPEC TO DB
@@ -520,7 +542,7 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 			int[] rewardList = {0};
 
 			//WRITE STIM-SPEC
-			NAFCStimSpecSpec stimSpec = new NAFCStimSpecSpec(targetEyeWinCoords.toArray(new Coordinates2D[0]), targetEyeWinSizeArray, sampleId, choiceIds, eStimObjData, rewardPolicy, rewardList);
+			NAFCStimSpecSpec stimSpec = new NAFCStimSpecSpec(targetEyeWinCoords.toArray(new Coordinates2D[0]), targetEyeWinSizeArray, sampleIds.get(i), choiceIds, eStimObjData, rewardPolicy, rewardList);
 
 			//WRITE TRIAL DATA
 			NoisyMStickNAFCRandTrialGenData genData = new NoisyMStickNAFCRandTrialGenData(numQMDistractors+numRandDistractors, 
@@ -529,7 +551,7 @@ public class NoisyMStickPngRandBlockGen extends NAFCBlockGen{
 					new double[] {choiceRadiusLowerLim, choiceRadiusUpperLim}, new double[] {distractorDistanceLowerLim, distractorDistanceUpperLim});
 
 
-			NoisyMStickNAFCRandTrialData trialData = new NoisyMStickNAFCRandTrialData(genData, noiseData, qmps, mmp);
+			NoisyMStickNAFCRandTrialData trialData = new NoisyMStickNAFCRandTrialData(genData, noiseData.get(i), qmps.get(i), mmps.get(i));
 
 
 			dbUtil.writeStimSpec(taskId, stimSpec.toXml(), trialData.toXml());
