ded70575181f5f96d846eb1cfcb16fb4eaf13101
Allen Chen <allenmuhanchen@gmail.com>
2022-07-05 07:04:15 -0400

    not working commit

---
+++ xper-train/xper-allen/test/org/xper/allen/nafc/blockgen/test/NoisyMStickPngRandBlockGenTest.java
@@ -0,0 +1,14 @@
+package org.xper.allen.nafc.blockgen.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NoisyMStickPngRandBlockGenTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
