f984e723210089f658c9a8b5f49597dd32688e4e
Allen Chen <allenmuhanchen@gmail.com>
2022-07-28 15:52:35 -0400

    trials now build up list of dict

--- EStimShapeAnalysis/src/data/test_trial.py
+++ EStimShapeAnalysis/src/data/test_trial.py
@@ -8,19 +8,20 @@ import timeutil
 class DuckField:
     def __init__(self, quack):
         self.quack = quack
+        self.name = "Duck"
     def getValue(self, when):
         self.value = self.quack
 
 class SubField(trial.Field):
     pass
 
-class StartField():
-    def __init__(self):
-        pass
+class StartField(trial.Field):
     def getValue(self, when):
         self.value = when.start
 
-class WhenField():
+class WhenField(trial.Field):
+    def __init__(self):
+        self.name = "tstamps"
     def getValue(self, when):
         self.value = when.tuple()
 
@@ -35,19 +36,20 @@ class TestField(TestCase):
         start1 = StartField()
         when1 = WhenField()
         d1 = DuckField("duck1")
-        d2 = DuckField("duck2")
         s1 = SubField()
 
         ## Interface for adding fields to a trial
-        for t in trialList:
-            t.add_field(when1)
-            t.add_field(start1)
-            t.add_field(d1)
-            t.add_field(d2)
-            t.add_field(s1)
+        fieldList = trial.FieldList()
+        fieldList.append(when1)
+        fieldList.append(start1)
+        fieldList.append(d1)
+        fieldList.append(s1)
 
-        df = pd.DataFrame()
         for t in trialList:
-            df = t.append_to_data_frame(df)
+            t.set_fields(fieldList)
 
-        print(df)
+        data = []
+
+        for t in trialList:
+            data = t.append_to_data(data)
+        print(pd.DataFrame(data))
