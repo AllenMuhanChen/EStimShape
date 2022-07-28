e9e83980a1cb00f86593b747ad1be21293c0d6ee
Allen Chen <allenmuhanchen@gmail.com>
2022-07-28 18:23:14 -0400

    We have %correct!

--- EStimShapeAnalysis/src/compile/psychometric_compile.py
+++ EStimShapeAnalysis/src/compile/psychometric_compile.py
@@ -4,41 +4,60 @@
 
 import pandas as pd
 import numpy as np
-from src.data import trial_field as tf
-from src.data import timeutil
-from src.data import table_util
+from src.data import trial_field as tf, timeutil, table_util, reader, trialcollector
 
-class TrialTypeField(tf.Field):
+
+class IsCorrectField(tf.Field):
     def __init__(self, beh_msg: pd.DataFrame):
         self.beh_msg = beh_msg
-        self.name = "TrialType"
+        self.name = "IsCorrect"
 
     def getValue(self, when: timeutil.When):
-        time_cond = table_util.within_tstamps(self.beh_msg, when)
+        time_cond = table_util.get_within_tstamps(self.beh_msg, when)
         correct = self.__get_num_corrects(time_cond)
         incorrect = self.__get_num_incorrects(time_cond)
         if correct == 1 and incorrect == 0:
-            return True
-        elif incorrect == 1 and correct == 1:
-            return False
+            self.value = True
+        elif incorrect == 1 and correct == 0:
+            self.value = False
         elif correct > 1 or incorrect > 1:
             raise ValueError("There's more than one choice in this trial!")
         else:
+            print(correct)
+            print(incorrect)
             raise ValueError("There's no choice in this trial!")
 
+
     def __get_num_incorrects(self, time_cond):
-        incorrect_cond = self.beh_msg['type'] == "ChoiceSelectionIncorrect"
-        incorrect = sum(incorrect_cond & time_cond)
+        incorrect_trials = self.beh_msg['type'] == "ChoiceSelectionIncorect"
+        incorrect = sum(np.logical_and(incorrect_trials, time_cond))
         return incorrect
 
     def __get_num_corrects(self, time_cond):
-        correct_cond = self.beh_msg['type'] == "ChoiceSelectionCorrect"
-        correct = sum(correct_cond & time_cond)
+        correct_trials = self.beh_msg['type'] == "ChoiceSelectionCorrect"
+        correct = sum(np.logical_and(correct_trials, time_cond))
         return correct
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    reader.honk()
+    beh_msg = trialcollector.beh_msg
+    trial_whens = trialcollector.collect_choice_trials()
+    trialList = []
+    for when in trial_whens:
+        trialList.append(tf.Trial(when))
 
+    fieldList = tf.FieldList()
+    fieldList.append(IsCorrectField(beh_msg))
+
+    for t in trialList:
+        t.set_fields(fieldList)
+
+    data = []
+
+    for t in trialList:
+        data = t.append_to_data(data)
+
+    df = pd.DataFrame(data)
+    print(sum(df["IsCorrect"])/len(df.index))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- EStimShapeAnalysis/src/data/table_util.py
+++ EStimShapeAnalysis/src/data/table_util.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-def within_tstamps(beh_msg, when):
+def get_within_tstamps(beh_msg, when):
     later_than_start = beh_msg['tstamp'] >= when.start
     before_stop = beh_msg['tstamp'] <= when.stop
     return np.logical_and(later_than_start, before_stop)
+
+def contains_success(beh_msg, when)->bool:
+    msg_type = beh_msg['type'].where(get_within_tstamps(beh_msg, when))
+    if msg_type.isin(["ChoiceSelectionSuccess"]).any():
+        return True
+    else:
+        return False
