b088d1e825105a8dd8047d35d9128e8a4f7a0303
Allen Chen <allenmuhanchen@gmail.com>
2022-07-28 15:52:35 -0400

    trials now build up list of dict

--- EStimShapeAnalysis/src/data/trial.py
+++ EStimShapeAnalysis/src/data/trial.py
@@ -1,46 +1,41 @@
 import pandas as pd
 import timeutil
+from collections import OrderedDict
 
 class Field:
     def __init__(self):
         self.name = type(self).__name__
+
     def getValue(self, when):
         self.value = self.name
 
+
 class FieldList(list):
-    pass
+    def get_df(self):
+        df = pd.DataFrame(columns=self.get_names())
+        return df
+    def get_names(self):
+        return [field.name for field in self]
+
 
 class Trial:
 
     def __init__(self, when):
-        self.fields = []
+        self.fields = FieldList()
         self.when = when
 
-    def add_field(self, field:Field):
-        self.fields.append(field)
+    def set_fields(self, fields: FieldList):
+        self.fields = fields
 
-    def append_to_data_frame(self, df:pd.DataFrame):
+    def append_to_data(self, data):
         self.__get_field_values()
-        # df.iloc[-1:] = [i.value for i in self.fields]
-        df = df.append(pd.Series([i.value for i in self.fields]), ignore_index=True)
-        return df
+        new_values = [i.value for i in self.fields]
+        names = self.fields.get_names()
+        new_row = OrderedDict(zip(names, new_values))
+        data.append(new_row)
+
+        return data
 
     def __get_field_values(self):
         for field in self.fields:
             field.getValue(self.when)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
