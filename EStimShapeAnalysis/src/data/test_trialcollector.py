8dc86cd15c6bd14bf842089aa3362b4a18973316
Allen Chen <allenmuhanchen@gmail.com>
2022-08-11 13:59:32 -0400

    resolve merge issues

--- EStimShapeAnalysis/src/data/test_trialcollector.py
+++ EStimShapeAnalysis/src/data/test_trialcollector.py
@@ -1,23 +1,20 @@
 from unittest import TestCase
 from src.data.trialcollector import TrialCollector
 from src.data.connection import Connection
+from src.data import timeutil
 
 class TestTrialCollectorMethods(TestCase):
+    conn = Connection("allen_estimshape_train_220725", when=timeutil.all())
+    trialcollector = TrialCollector(conn)
     def test_collect_trials(self):
-        conn = Connection("allen_estimshape_train_220725")
-        trialcollector = TrialCollector(conn)
-        trial_whens = trialcollector.collect_trials()
+
+        trial_whens = self.trialcollector.collect_trials()
         actualNumCorrect = sum([True for i in trial_whens if (i.start<i.stop)])
         expectedNumCorrect = len(trial_whens)
         self.assertEqual(expectedNumCorrect, actualNumCorrect)
 
     def test_collect_choices_trials(self):
-<<<<<<< HEAD
-        conn = Connection("allen_estimshape_train_220725")
-        trialcollector = TrialCollector(conn)
-=======
->>>>>>> 56d5c669cd40f00458aeb689625709c0bf48ea12
-        trial_whens = trialcollector.collect_choice_trials()
+        trial_whens = self.trialcollector.collect_choice_trials()
         actualNumCorrect = sum([True for i in trial_whens if (i.start<i.stop)])
         expectedNumCorrect = len(trial_whens)
         self.assertEqual(expectedNumCorrect, actualNumCorrect)
--- EStimShapeAnalysis/src/data/trialcollector.py
+++ EStimShapeAnalysis/src/data/trialcollector.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-<<<<<<< HEAD
 from src.data import connection, timeutil, table_util
 
 
@@ -9,29 +8,6 @@ class TrialCollector:
         self.beh_msg = conn.beh_msg
         self.stim_spec = conn.stim_spec
         self.stim_obj_data = conn.stim_obj_data
-=======
-from src.data import reader, timeutil, table_util
-
-beh_msg = reader.get_beh_msg()
-stim_spec = reader.get_stim_sec()
-def collect_choice_trials():
-    all_trial_whens = collect_trials()
-    choice_trial_whens = []
-    for when in all_trial_whens:
-        if table_util.contains_success(beh_msg, when):
-            choice_trial_whens.append(when)
-    return choice_trial_whens
-
-
-
-def collect_trials():
-    trial_starts = beh_msg[beh_msg['type'] == "TrialStart"]['tstamp'].values
-    trial_stops = beh_msg[beh_msg['type'] == "TrialStop"]['tstamp'].values
-    trial_starts, trial_stops = __ensure_ends_are_aligned(trial_starts, trial_stops)
-    trial_starts, trial_stops = __ensure_balanced_trial_nums(trial_starts, trial_stops)
-    trial_starts, trial_stops = __remove_misaligned_trials(trial_starts, trial_stops)
-    return [timeutil.When(trial_starts[i], trial_stops[i]) for i in range(len(trial_starts))]
->>>>>>> 56d5c669cd40f00458aeb689625709c0bf48ea12
 
     def collect_choice_trials(self):
         all_trial_whens = self.collect_trials()
@@ -58,7 +34,6 @@ def collect_trials():
             trial_starts = trial_starts[:-1]
         return trial_starts, trial_stops
 
-<<<<<<< HEAD
     def __ensure_balanced_trial_nums(self, trial_starts, trial_stops):
         while trial_starts.size != trial_stops.size:
             if trial_starts.size > trial_stops.size:
@@ -83,52 +58,17 @@ def collect_trials():
             trial_starts = np.delete(trial_starts, first_bad_trial)
             trial_stops = np.delete(trial_stops, first_bad_trial)
         return trial_starts, trial_stops
-=======
-def __ensure_balanced_trial_nums(trial_starts, trial_stops):
-    while trial_starts.size != trial_stops.size:
-        if trial_starts.size > trial_stops.size:
-            diff_length = trial_starts.size - trial_stops.size
-            first_bad_trial = __get_first_bad_trial(trial_starts[:-diff_length], trial_stops)
-            trial_starts = np.delete(trial_starts, first_bad_trial)
-        else:
-            diff_length = trial_stops.size - trial_starts.size
-            first_bad_trial = __get_first_bad_trial(trial_starts, trial_stops[:-diff_length])
-            trial_stops = np.delete(trial_stops, first_bad_trial)
-    return trial_starts, trial_stops
-
-
-def __remove_misaligned_trials(trial_starts, trial_stops):
-    while not __trials_aligned(trial_starts, trial_stops):
-        first_bad_trial = __get_first_bad_trial(trial_starts, trial_stops)
-        trial_starts = np.delete(trial_starts, first_bad_trial)
-        trial_stops = np.delete(trial_stops, first_bad_trial)
-    return trial_starts, trial_stops
-
-
-def __get_first_bad_trial(trial_starts, trial_stops):
-    bad_trials = np.array(trial_starts > trial_stops)
-    first_bad_trial = [i for i, x in enumerate(bad_trials) if x][0]
-    return first_bad_trial
-
-
-def __trials_aligned(trial_starts, trial_stops):
-    actual_correctly_aligned = sum([True for i in range(len(trial_starts)) if (trial_starts[i] < trial_stops[i])])
-    return actual_correctly_aligned == len(trial_starts)
->>>>>>> 56d5c669cd40f00458aeb689625709c0bf48ea12
 
     def __get_first_bad_trial(self, trial_starts, trial_stops):
         bad_trials = np.array(trial_starts > trial_stops)
         first_bad_trial = [i for i, x in enumerate(bad_trials) if x][0]
         return first_bad_trial
 
-<<<<<<< HEAD
     def __trials_aligned(self, trial_starts, trial_stops):
         actual_correctly_aligned = sum([True for i in range(len(trial_starts)) if (trial_starts[i] < trial_stops[i])])
         return actual_correctly_aligned == len(trial_starts)
 
 
-=======
->>>>>>> 56d5c669cd40f00458aeb689625709c0bf48ea12
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     pass
