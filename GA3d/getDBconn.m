3673a9260a2f15ec9d4d8954ee14a040924ab1c7
Allen Chen <allenmuhanchen@gmail.com>
2022-08-16 13:19:05 -0400

    File reorga

--- GA3d/getDBconn.m
+++
@@ -1,9 +0,0 @@
-function conn = getDBconn(folderName)
-    databaseName = 'ram_180616_3dma';
-    serverAddress = '172.30.6.27';
-%     serverAddress = 'localhost';
-    conn = database(databaseName,'xper_rw','up2nite','Vendor','MySQL','Server',serverAddress);
-    if exist('folderName','var')
-        logger(mfilename,folderName,['Connected to MySQL database: ' databaseName ' on ' serverAddress '.'],conn);
-    end
-end
--- GA3d/getShapeColor.m
+++
@@ -1,4 +0,0 @@
-function color = getShapeColor(fColor)
-    colorIdx = datasample(1:length(fColor.options),1,'weights',fColor.prob);
-    color = fliplr(de2bi(fColor.options(colorIdx),3));
-end
