6533b36553b737de2698b8eaa55ba31a00d4f2c5
Allen Chen <allenmuhanchen@gmail.com>
2022-08-16 16:27:01 -0400

    added try catch to logger

--- GA3D/src/logger.m
+++ GA3D/src/logger.m
@@ -4,9 +4,13 @@ function logger(from,exptId,message,conn)
     str = [num2str(tstamp) ': ' from ': ' message '\n'];
 
     fileID = fopen([logPath '/' exptId '.txt'],'a');
-    fileID
-    fprintf(fileID,str);
-    fclose(fileID);
+    try
+        fprintf(fileID,str);
+        fclose(fileID);
+    catch e
+        disp("Warning, couldn't save log")
+    end 
+    
     
     insertIntoSqlTable({tstamp [exptId ': ' from ': ' message]},{'tstamp','memo'},'ExpLog',conn);
 end
