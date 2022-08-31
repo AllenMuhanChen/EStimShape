e09e6a7c8250f9cfcfff5e59b4e0fa8c3eb7b143
Allen Chen <allenmuhanchen@gmail.com>
2022-08-31 16:06:19 -0400

    AcqDataTest now uses TestConfig to get DataSource

--- xper-train/xper/unit_test/org/xper/acq/AcqDataTest.java
+++ xper-train/xper/unit_test/org/xper/acq/AcqDataTest.java
@@ -5,26 +5,29 @@ import java.util.Map;
 
 import junit.framework.TestCase;
 
+import org.springframework.config.java.context.JavaConfigApplicationContext;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.xper.acq.vo.DigitalChannel;
 import org.xper.db.vo.AcqDataEntry;
 import org.xper.db.vo.SystemVariable;
 import org.xper.util.DbUtil;
+import org.xper.util.FileUtil;
 
 public class AcqDataTest extends TestCase {
 	public void testReadAcqData () {
-		DriverManagerDataSource dataSource;
-		DbUtil dbUtil;
-		dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//		dataSource.setUrl("jdbc:mysql://localhost/wang");
-		dataSource.setUrl("jdbc:mysql://localhost/ram_160321_stim");
-		dataSource.setUsername("xper_rw");
-		dataSource.setPassword("up2nite");
+//		DriverManagerDataSource dataSource;
+//		dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+////		dataSource.setUrl("jdbc:mysql://localhost/wang");
+//		dataSource.setUrl("jdbc:mysql://localhost/ram_160321_stim");
+//		dataSource.setUsername("xper_rw");
+//		dataSource.setPassword("up2nite");
+//
+//		dbUtil = new DbUtil();
+//		dbUtil.setDataSource(dataSource);
+		JavaConfigApplicationContext context = new JavaConfigApplicationContext(FileUtil.loadConfigClass("test.experiment.config_class"));
+		DbUtil dbUtil = context.getBean(DbUtil.class);
 
-		dbUtil = new DbUtil();
-		dbUtil.setDataSource(dataSource);
-		
 		long ts = dbUtil.readAcqDataMaxTimestamp();
 		Map<String,SystemVariable> vars = dbUtil.readSystemVar("%", ts);
 		int maxChannel = Integer.parseInt(vars.get("acq_n_channel").getValue(0));
