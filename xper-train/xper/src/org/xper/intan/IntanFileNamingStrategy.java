7e92c30fa8b288c56eb9161b56dca8a1afcdc6e6
Allen Chen <allenmuhanchen@gmail.com>
2022-09-21 15:38:12 -0400

    TrialStop starts playback but ends recording

--- xper-train/xper/src/org/xper/intan/IntanFileNamingStrategy.java
+++ xper-train/xper/src/org/xper/intan/IntanFileNamingStrategy.java
@@ -3,6 +3,14 @@ package org.xper.intan;
 import org.xper.Dependency;
 import org.xper.classic.vo.TrialContext;
 
+/**
+ * @author Allen Chen
+ *
+ * To make your own NamingStrategy, make a new class that extends this one
+ * and implement rename(). You can use context to get any information you need
+ * about the trial/experiment and IntanController to get the pre-set savePath and
+ * baseFilename.
+ */
 public abstract class IntanFileNamingStrategy {
     @Dependency
     IntanController intanController;
