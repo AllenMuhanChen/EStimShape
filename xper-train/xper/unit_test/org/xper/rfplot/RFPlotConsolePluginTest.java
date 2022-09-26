73c0f069df14dd0fc26d58e3dd415b7bace52dcd
Allen Chen <allenmuhanchen@gmail.com>
2022-09-26 14:57:34 -0400

    start uses default stim spec
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

---
+++ xper-train/xper/unit_test/org/xper/rfplot/RFPlotConsolePluginTest.java
@@ -0,0 +1,38 @@
+package org.xper.rfplot;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.xper.rfplot.gui.RFPlotConsolePlugin;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class RFPlotConsolePluginTest {
+
+    private static RFPlotConsolePlugin plugin;
+    private static MockRFPlotTaskDataSourceClient client;
+    private static HashMap<String, RFPlotDrawable> rfObjectMap;
+
+    @BeforeClass
+    public static void setUp(){
+        client = new MockRFPlotTaskDataSourceClient();
+
+        rfObjectMap = new HashMap<>();
+        rfObjectMap.put(RFPlotGaborObject.class.getName(), new RFPlotGaborObject());
+
+        plugin = new RFPlotConsolePlugin();
+        plugin.setClient(client);
+        plugin.setRfObjectMap(rfObjectMap);
+
+    }
+
+    @Test
+    public void start_plugin_sets_default_stim_spec(){
+        plugin.startPlugin();
+
+        String actualStimSpec = client.getMockStim();
+        String expectedStimSpec = new RFPlotGaborObject().getSpec();
+        assertEquals(expectedStimSpec, actualStimSpec);
+    }
+}
