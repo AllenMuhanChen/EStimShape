2d722422ac193f3e61123f668d50c12a9bbf5d38
Allen Chen <allenmuhanchen@gmail.com>
2022-10-31 15:24:44 -0400

    Fixed sort_fix trial_collector

--- EStimShapeAnalysis/src/compile/trial_collector.py
+++ EStimShapeAnalysis/src/compile/trial_collector.py
@@ -21,17 +21,15 @@ class TrialCollector:
     def collect_trials(self):
         trial_starts = self.beh_msg[self.beh_msg['type'] == "TrialStart"]['tstamp'].values
         trial_stops = self.beh_msg[self.beh_msg['type'] == "TrialStop"]['tstamp'].values
-        # trial_starts, trial_stops = self.fix_bad_trials(trial_starts, trial_stops)
         trial_starts, trial_stops = self.sort_fix_bad_trials(trial_starts, trial_stops)
         return [time_util.When(trial_starts[i], trial_stops[i]) for i in range(len(trial_starts))]
 
-    def fix_bad_trials(self, trial_starts, trial_stops):
-        trial_starts, trial_stops = self.__ensure_ends_are_aligned(trial_starts, trial_stops)
-        trial_starts, trial_stops = self.__ensure_balanced_trial_nums(trial_starts, trial_stops)
-        trial_starts, trial_stops = self.__remove_misaligned_trials(trial_starts, trial_stops)
-        return trial_starts, trial_stops
-
     def sort_fix_bad_trials(self, trial_starts, trial_stops):
+        """Finds bad trials by sorting the trial start and stops by their time stamps
+        in the same array. We should have start, stop, start, stop, ..., and so on.
+        If there's a duplicate start (i.e start, start, stop): then we delete the earlier trial_start
+        If there's a duplicate stop (i.e, start, stop, stop):  then we delete the later trial_stop
+        """
         self.__ensure_ends_are_aligned(trial_starts, trial_stops)
 
         trial_starts_ids = ["trial_start" for trial_start in trial_starts]
@@ -49,7 +47,8 @@ class TrialCollector:
                     to_remove.append(i - 1)  # remove earlier trial_start, since it's missing a trial_stop
                     print("removing duplicate trial_start")
                 else:
-                    print("Somehow we have two repeating stops!")
+                    print("Somehow we have two repeating stops!: " + str(i))
+                    to_remove.append(i)
             previous = item[0]
         combined_sorted_removed = [item for i, item in enumerate(combined_sorted) if i in to_remove]
         combined_sorted = [item for i, item in enumerate(combined_sorted) if i not in to_remove]
