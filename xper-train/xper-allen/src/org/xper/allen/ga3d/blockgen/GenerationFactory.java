39ef5d16138b47c1107bd7a8902c4217c0b95147
Allen Chen <allenmuhanchen@gmail.com>
2022-11-01 17:01:55 -0400

    refractor package to ga3d.blockgen
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/src/org/xper/allen/ga3d/blockgen/GenerationFactory.java
+++ xper-train/xper-allen/src/org/xper/allen/ga3d/blockgen/GenerationFactory.java
@@ -1,10 +1,13 @@
-package org.xper.allen.nafc.blockgen.ga;
+package org.xper.allen.ga3d.blockgen;
 
 import org.xper.allen.nafc.blockgen.Trial;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public class GenerationFactory {
     public List<Trial> createFirstGenerationTrials(){
+        List<Trial> trials = new LinkedList<>();
 
     }
 
