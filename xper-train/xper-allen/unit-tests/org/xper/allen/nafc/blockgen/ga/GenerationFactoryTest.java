122ff37dd694d4e15629ed5e170c98003d9a126b
Allen Chen <allenmuhanchen@gmail.com>
2022-11-01 16:57:34 -0400

    Psuedocode for GenerationFactory
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

---
+++ xper-train/xper-allen/unit-tests/org/xper/allen/nafc/blockgen/ga/GenerationFactoryTest.java
@@ -0,0 +1,20 @@
+package org.xper.allen.nafc.blockgen.ga;
+
+import org.junit.Test;
+import org.xper.allen.nafc.blockgen.Trial;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class GenerationFactoryTest {
+
+    @Test
+    public List<Trial> createFirstGeneration() {
+        GenerationFactory factory = new GenerationFactory();
+
+    }
+
+    @Test
+    public void addNthGeneration() {
+    }
+}
