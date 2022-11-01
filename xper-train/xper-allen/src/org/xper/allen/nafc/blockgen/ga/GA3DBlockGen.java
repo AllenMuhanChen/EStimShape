09b360ac75128aa1d55cc80cb3a575beb61a979b
Allen Chen <allenmuhanchen@gmail.com>
2022-11-01 16:41:51 -0400

    GA3DBlockGen pseudocode
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/src/org/xper/allen/nafc/blockgen/ga/GA3DBlockGen.java
+++ xper-train/xper-allen/src/org/xper/allen/nafc/blockgen/ga/GA3DBlockGen.java
@@ -11,26 +11,30 @@ public class GA3DBlockGen extends AbstractMStickPngTrialGenerator {
     Long genId;
     private List<Trial> trials = new LinkedList<>();
 
-//    public void generate(){
-//        pngMaker.createDrawerWindow();
-//
-//        if(firstGeneration()){
-//            addFirstGeneration();
-//        } else{
-//            addNthGeneration();
-//            //add:
-//            // 1. Compile neural responses then choose stimuli to morph
-//            // 2. Assign stimulus type (rand, or child)
-//        }
-//        preWriteTrials();
-//        shuffleTrials();
-//
-//        if(firstGeneration()){
-//            writeFirstGeneration();
-//        } else{
-//            writeNthGeneration();
-//        }
-//
-//    }
+    public void generate(){
+        pngMaker.createDrawerWindow();
+
+        if(firstGeneration()){
+            addFirstGeneration();
+        } else{
+            addNthGeneration();
+            //add:
+            // 1. Compile neural responses then choose stimuli to morph
+            // 2. Assign stimulus type (rand, or child)
+        }
+        preWriteTrials();
+        shuffleTrials();
+
+        if(firstGeneration()){
+            writeFirstGeneration();
+        } else{
+            writeNthGeneration();
+        }
+
+        dbUtil.updateReadyGenerationInfo(genId, trials.size());
+        System.out.println("Done Generating...");
+        pngMaker.close();
+
+    }
 
 }
