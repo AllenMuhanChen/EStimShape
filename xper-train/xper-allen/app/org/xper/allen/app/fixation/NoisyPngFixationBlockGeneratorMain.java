747d36600308bdcac1a91d93a9e9e482eef02c37
Allen Chen <allenmuhanchen@gmail.com>
2022-11-07 17:27:07 -0500

    Finished, but untested
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

---
+++ xper-train/xper-allen/app/org/xper/allen/app/fixation/NoisyPngFixationBlockGeneratorMain.java
@@ -0,0 +1,60 @@
+package org.xper.allen.app.fixation;
+
+import org.springframework.config.java.context.JavaConfigApplicationContext;
+import org.xper.allen.app.nafc.TrialArgReader;
+import org.xper.allen.fixation.blockgen.NoisyPngFixationBlockGen;
+import org.xper.allen.fixation.blockgen.NoisyPngFixationBlockParameters;
+import org.xper.allen.nafc.blockgen.Lims;
+import org.xper.allen.nafc.blockgen.NoiseFormer;
+import org.xper.allen.nafc.blockgen.TypeFrequency;
+import org.xper.allen.nafc.vo.NoiseParameters;
+import org.xper.allen.nafc.vo.NoiseType;
+import org.xper.util.FileUtil;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class NoisyPngFixationBlockGeneratorMain {
+
+    public static void main(String[] args) {
+        JavaConfigApplicationContext context = new JavaConfigApplicationContext(
+                FileUtil.loadConfigClass("experiment.ga.config_class"));
+
+        NoisyPngFixationBlockGen gen = context.getBean(NoisyPngFixationBlockGen.class);
+
+        try {
+            gen.setUp(NoisyPngFixationBlockGenInputTranslator.translate(args));
+            gen.generate();
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Failed to generate trials");
+        }
+
+    }
+
+    public static class NoisyPngFixationBlockGenInputTranslator extends TrialArgReader {
+
+        public static NoisyPngFixationBlockParameters translate(String[] args){
+            List<String> argsList = Arrays.asList(args);
+            ListIterator<String> iterator = argsList.listIterator();
+
+            int numTrials = Integer.parseInt(iterator.next());
+            TypeFrequency<Lims> randNoiseChancesTF = new TypeFrequency<>(nextLimsType(), nextFrequency());
+            TypeFrequency<NoiseType> noiseTypesTF = new TypeFrequency<>(stringToNoiseTypes(iterator.next()), nextFrequency());
+            Lims distanceLims = stringToLim(iterator.next());
+            double size = Double.parseDouble(iterator.next());
+
+            List<NoiseType> noiseTypes = noiseTypesTF.getShuffledTrialList(numTrials);
+            List<Lims> noiseChances = randNoiseChancesTF.getShuffledTrialList(numTrials);
+            List<NoiseParameters> noiseParameters = new LinkedList<>();
+            for(int i = 0; i< numTrials; i++){
+                noiseParameters.add(new NoiseParameters(NoiseFormer.getNoiseForm(noiseTypes.get(i)), noiseChances.get(i)));
+            }
+
+            return new NoisyPngFixationBlockParameters(noiseParameters, distanceLims, size);
+        }
+
+    }
+}
--- xper-train/xper-allen/src/org/xper/allen/fixation/blockgen/NoisyPngFixationBlockGen.java
+++ xper-train/xper-allen/src/org/xper/allen/fixation/blockgen/NoisyPngFixationBlockGen.java
@@ -1,27 +1,24 @@
 package org.xper.allen.fixation.blockgen;
 
 import org.xper.allen.nafc.blockgen.AbstractMStickPngTrialGenerator;
-import org.xper.allen.nafc.blockgen.Trial;
+import org.xper.allen.nafc.vo.NoiseParameters;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class NoisyPngFixationBlockGen extends AbstractMStickPngTrialGenerator {
 
-    private NoisyPngFixationParameters params;
-    private double numTrials;
+    private NoisyPngFixationBlockParameters params;
 
-    public NoisyPngFixationBlockGen(NoisyPngFixationParameters params, double numTrials) {
+    public void setUp(NoisyPngFixationBlockParameters params) {
         this.params = params;
-        this.numTrials = numTrials;
     }
 
     @Override
     protected void addTrials() {
-        List<Trial> trials = new LinkedList<>();
-
-        for(int i = 0; i< numTrials; i++){
-            trials.add(new NoisyPngFixationTrial(this, params));
+        List<NoiseParameters> noises = params.getNoiseParameters();
+        for (NoiseParameters noise : noises) {
+            NoisyPngFixationTrialParameters trialParams = new NoisyPngFixationTrialParameters(noise, params.getDistanceLims(), params.getSize());
+            trials.add(new NoisyPngFixationTrial(this, trialParams));
         }
     }
 
