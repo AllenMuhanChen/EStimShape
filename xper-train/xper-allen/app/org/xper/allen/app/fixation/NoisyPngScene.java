c064f51d016f9679840e885bb928e62de2939299
Allen Chen <allenmuhanchen@gmail.com>
2022-11-08 10:23:37 -0500

    Fixed, works, change NoisyTranslatableResizableImages to handle its own noiseIndx, and some refractors to NoisyPngScene and NAFCNosiyPngScene due to changes
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/app/org/xper/allen/app/fixation/NoisyPngScene.java
+++ xper-train/xper-allen/app/org/xper/allen/app/fixation/NoisyPngScene.java
@@ -28,6 +28,7 @@ public class NoisyPngScene extends AbstractTaskScene{
     private ImageDimensions stimDimensions;
     private int noiseIndx;
     private int numNoiseFrames;
+    private static final int bufferFrames=10;
 
     @Override
     public void initGL(int w, int h) {
@@ -42,10 +43,7 @@ public class NoisyPngScene extends AbstractTaskScene{
     @Override
     public void trialStart(TrialContext context){
         double durationSeconds = getSlideLength() /1000.0;
-        numNoiseFrames = (int) Math.ceil((durationSeconds * getFrameRate()));
-        image = new NoisyTranslatableResizableImages(numNoiseFrames, 1 );
-        image.initTextures();
-        noiseIndx = 0;
+        numNoiseFrames = (int) Math.ceil((durationSeconds * getFrameRate())) + bufferFrames;
     }
 
     public double[] getBackgroundColor() {
@@ -62,12 +60,20 @@ public class NoisyPngScene extends AbstractTaskScene{
 
     @Override
     public void setTask(ExperimentTask task) {
+        newNoisyPng();
+
         NoisyPngSpec spec = NoisyPngSpec.fromXml(task.getStimSpec());
         stimLocation = new Coordinates2D(spec.getxCenter(), spec.getyCenter());
         stimDimensions = spec.getImageDimensions();
         image.loadTexture(spec.getPath(),0);
         String noiseMapPath = spec.getNoiseMapPath();
-        image.loadNoise(noiseMapPath,0);
+        image.loadNoise(noiseMapPath);
+    }
+
+    private void newNoisyPng() {
+        image = new NoisyTranslatableResizableImages(numNoiseFrames, 1 );
+        image.initTextures();
+        noiseIndx = 0;
     }
 
     @Override
@@ -81,8 +87,7 @@ public class NoisyPngScene extends AbstractTaskScene{
                     GL11.glStencilFunc(GL11.GL_EQUAL, 0, 1);
                 }
                 int pngIndex = 0; //Should be zero
-                image.draw(noiseIndx, context, pngIndex, stimLocation, stimDimensions);
-                nextNoise();
+                image.draw(true, context, pngIndex, stimLocation, stimDimensions);
                 if (useStencil) {
                     // 1 will pass for fixation and marker regions
                     GL11.glStencilFunc(GL11.GL_EQUAL, 1, 1);
@@ -98,13 +103,6 @@ public class NoisyPngScene extends AbstractTaskScene{
             }}, context);
     }
 
-    public void nextNoise() {
-        if (noiseIndx + 1 > numNoiseFrames)
-            noiseIndx=0;
-        this.noiseIndx++;
-        System.out.println(noiseIndx);
-        System.out.println(numNoiseFrames);
-    }
 
     @Override
     public void trialStop(TrialContext context) {
