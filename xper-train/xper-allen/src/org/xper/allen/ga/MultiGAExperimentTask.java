34185d4ba2fde4e917c229ddc679222ca47af048
Allen Chen <allenmuhanchen@gmail.com>
2022-11-10 16:40:45 -0500

    Working tests on MultiGATaskDAtaSource
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

---
+++ xper-train/xper-allen/src/org/xper/allen/ga/MultiGAExperimentTask.java
@@ -0,0 +1,15 @@
+package org.xper.allen.ga;
+
+import org.xper.experiment.ExperimentTask;
+
+public class MultiGAExperimentTask extends ExperimentTask {
+    String gaName;
+
+    public String getGaName() {
+        return gaName;
+    }
+
+    public void setGaName(String gaName) {
+        this.gaName = gaName;
+    }
+}
