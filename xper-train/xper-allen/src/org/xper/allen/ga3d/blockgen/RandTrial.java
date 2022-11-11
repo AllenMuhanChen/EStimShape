2f9f1f05ee6b706a17e30cbd3ee6b6ef6c72502b
Allen Chen <allenmuhanchen@gmail.com>
2022-11-11 13:43:04 -0500

    Simple rand analysis

--- xper-train/xper-allen/src/org/xper/allen/ga3d/blockgen/RandTrial.java
+++ xper-train/xper-allen/src/org/xper/allen/ga3d/blockgen/RandTrial.java
@@ -1,6 +1,8 @@
 package org.xper.allen.ga3d.blockgen;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 import org.xper.allen.Trial;
+import org.xper.drawing.stick.MatchStick;
 
 public class RandTrial implements Trial {
     @Override
@@ -10,7 +12,13 @@ public class RandTrial implements Trial {
 
     @Override
     public void write() {
+        MatchStick mStick = new MatchStick();
+        mStick.genMatchStickRand();
 
+        //size
+        //location
+        //color
+        //shading
     }
 
     @Override
