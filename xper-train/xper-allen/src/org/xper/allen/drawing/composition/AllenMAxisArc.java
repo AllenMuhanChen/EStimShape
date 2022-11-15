763e823c8adf834aaa23d26d26b832201ecd34ff
Allen Chen <allenmuhanchen@gmail.com>
2022-11-15 11:30:08 -0500

    Cleanup of MAXisArc Test
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/src/org/xper/allen/drawing/composition/AllenMAxisArc.java
+++ xper-train/xper-allen/src/org/xper/allen/drawing/composition/AllenMAxisArc.java
@@ -12,6 +12,7 @@ import org.xper.allen.drawing.composition.metricmorphs.MetricMorphParams;
 import org.xper.allen.drawing.composition.qualitativemorphs.QualitativeMorph;
 import org.xper.allen.drawing.composition.qualitativemorphs.QualitativeMorphParams;
 import org.xper.drawing.stick.MAxisArc;
+import org.xper.drawing.stick.stickMath_lib;
 
 /**
  * I created this class soley to be able to write a new genSiilarArc (genMetricSimilarArc) that
@@ -612,6 +613,164 @@ public class AllenMAxisArc extends MAxisArc {
 //		setLocalArcLen(upSampledLocalArcLen);
 //		setmTangent(upSampledMTangent);
 
+
+	}
+
+	public void genSimilarArc(AllenMAxisArc inArc,int alignedPt,  double volatileRate) {
+		boolean showDebug = false;
+		if ( showDebug)
+			System.out.println("In MAxisArc.genSimilarArc()");
+		double RadView = 5.0;
+		//double[] orientationAngleRange = { Math.PI/12.0 , Math.PI/6.0}; // 15 ~ 30 degree
+		// Nov 20th, the orientation change seems to be too large
+		// since this is used to generate similar tube, we should make it more narrow
+		double[] orientationAngleRange = { Math.PI/24.0 , Math.PI/12.0}; // 7.5 ~ 15 degree
+		boolean[] chgFlg = new boolean[5];
+		int i;
+		//possible parameters , 1. mAxisCurvature, 2.ArcLen 3. orientation 4. devAngle
+		// 0. decide what parameters to chg
+		while (true) {
+			for (i=1; i<=4; i++) {
+				chgFlg[i] = false;
+				if ( stickMath_lib.rand01() < volatileRate)
+					chgFlg[i] = true;
+			}
+			int a = 3;
+			if ( a == 3) break;
+			//debug
+			if (inArc.getRad() <= 0.6 * RadView) {
+				if ( chgFlg[1] != false || chgFlg[2] != false || chgFlg[3] != false || chgFlg[4] != false)
+					break;
+			}
+			else { // the in Arc is str8 line, then we don't want only devAngle chg, (which is not prominent) {
+				if ( chgFlg[1] !=false || chgFlg[2] !=false || chgFlg[3] !=false)
+					break;
+			}
+		}
+
+		if (showDebug) {
+			System.out.println("the modification of mAxis are:");
+			for (i=1; i<=4; i++) System.out.print(" "+ chgFlg[i]);
+			System.out.println("");
+		}
+
+
+		double newRad = inArc.getRad();
+		double newArcLen = inArc.getArcLen();
+		Vector3d newTangent = new Vector3d(inArc.getmTangent()[ inArc.getTransRotHis_rotCenter()]);
+		double newDevAngle = inArc.getTransRotHis_devAngle();
+
+		// 1. mAxisCurvature
+		if ( chgFlg[1] == true) {
+			double totalRange;
+			double oriRad = inArc.getRad();
+			if ( oriRad <= 0.6 * RadView )  { // origianlly small Rad arc
+				double[] prob = {0.5, 1.0};
+				int choice = stickMath_lib.pickFromProbDist( prob);
+				if ( choice == 1) {
+					while (true) {
+						newRad = (stickMath_lib.rand01() * 0.4 + 0.2) * RadView;
+						totalRange = 0.4 * RadView;
+						if ( Math.abs( newRad - oriRad) > 0.2 * totalRange )
+							break;
+					}
+				}
+				else // chg to medium curvature regime
+					newRad = (stickMath_lib.rand01() * 5.4 + 0.6) * RadView;
+			}
+			else if ( oriRad <= 6.0 * RadView) { // originall in medium regime
+				double[] prob = {0.25, 0.75, 1.0};
+				int choice = stickMath_lib.pickFromProbDist( prob);
+				if (choice == 1)
+					newRad = (stickMath_lib.rand01() * 0.4 + 0.2) * RadView;
+				else if ( choice == 2) {
+					while (true) {
+						newRad = (stickMath_lib.rand01() * 5.4 + 0.6) * RadView;
+						totalRange = 5.4 * RadView;
+						if ( Math.abs(newRad - oriRad) > 0.2 * totalRange)
+							break;
+					}
+				}
+				else if ( choice == 3)
+					newRad = 100000.0;
+			}
+			else {// str8 original curvature
+				//always chg to medium curvature
+				newRad = (stickMath_lib.rand01() * 5.4 + 0.6) * RadView;
+			}
+		} // mAxisCurvature if
+
+		// 2. ArcLen
+		if ( chgFlg[2] == true) {
+			double oriArcLen = inArc.getArcLen();
+			double length_lb = 2.0;
+			double length_ub = Math.min( Math.PI * newRad, RadView);
+			double l_range = length_ub - length_lb;
+			while (true) { //pick value btw length_lb, length_ub, but not very near or very far from original value
+				newArcLen = stickMath_lib.randDouble( length_lb, length_ub);
+				if ( oriArcLen > length_ub || oriArcLen < length_lb) // no need to nearby check
+					break;
+				if ( Math.abs( newArcLen - oriArcLen) >= 0.2 * l_range &&
+						Math.abs( newArcLen - oriArcLen) <= 0.4 * l_range )
+					break;
+			}
+		}
+
+		// 3. orientation
+		if ( chgFlg[3] == true) {
+			Vector3d oriTangent = new Vector3d( inArc.getmTangent()[inArc.getTransRotHis_rotCenter()]);
+			while (true) {
+				newTangent = stickMath_lib.randomUnitVec();
+				double angle = newTangent.angle(oriTangent);
+				if ( angle >= orientationAngleRange[0] && angle <= orientationAngleRange[1]) // 15 ~ 30 degree
+					break;
+			}
+		}
+		// 4. devAngle
+		if ( chgFlg[4] == true)
+		{
+			double oriDevAngle = inArc.getTransRotHis_devAngle();
+			double diff = stickMath_lib.randDouble( Math.PI/6.0, Math.PI/3.0); // this diff is btw  30 - 60 degree
+			if ( stickMath_lib.rand01() < 0.5)
+				newDevAngle = oriDevAngle - diff;
+			else
+				newDevAngle = oriDevAngle + diff;
+		}
+
+
+		// use the new required vlaue to generate and transROt the mAxisArc
+
+		this.genArc(newRad, newArcLen); // the variable will be saved in this function
+
+		Point3d finalPos = new Point3d( inArc.getmPts()[alignedPt]);
+		//
+		this.transRotMAxis( alignedPt, finalPos, inArc.getTransRotHis_rotCenter(), newTangent, newDevAngle);
+// 	Point3d finalPos = new Point3d(0.0,0.0,0.0);
+// 	this.transRotMAxis( 26, finalPos, inArc.transRotHis_rotCenter, newTangent, newDevAngle);
+
+		if (showDebug)
+		{
+			System.out.println("rad    : " + inArc.getRad() + " -> " + newRad);
+			System.out.println("arcLen : " + inArc.getArcLen() + " -> " + newArcLen);
+			System.out.println("ori    : " + inArc.getmTangent()[inArc.getTransRotHis_rotCenter()] + " -> " + newTangent);
+			System.out.println("	angle btw is " + inArc.getmTangent()[inArc.getTransRotHis_rotCenter()].angle(newTangent));
+			System.out.println("devAng : " + inArc.getTransRotHis_devAngle() + " -> " + newDevAngle);
+			int rotCenter = inArc.getTransRotHis_rotCenter();
+			System.out.println("ori rot center "+  rotCenter + " pos " + inArc.getmPts()[rotCenter] + "\ntan: "+ inArc.getmTangent()[rotCenter]);
+			System.out.println("NEW rot center "+  rotCenter + " pos " + this.getmPts()[rotCenter] + "\ntan: "+ this.getmTangent()[rotCenter]);
+			System.out.println("the input alignedPt is " + alignedPt);
+// 			for (i=1; i<=51; i++)
+// 			{
+// // 				double dist = mPts[i].distance( inArc.mPts[i]);
+// // 				if (dist > 0.01)
+// 						System.out.println("MPts["+i+"]: " + this.mPts[i] + " " + inArc.mPts[i]);
+//
+// 			}
+			//System.out.println("MPts[3]: " + this.mPts[3] + " " + inArc.mPts[3]);
+			//System.out.println("MPts[20]: " + this.mPts[20] + " " + inArc.mPts[20]);
+
+			System.out.println("");
+		}
 
 	}
 
--- xper-train/xper-allen/unit-tests/org/xper/allen/drawing/composition/AllenMAxisArcTest.java
+++ xper-train/xper-allen/unit-tests/org/xper/allen/drawing/composition/AllenMAxisArcTest.java
@@ -16,6 +16,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AllenMAxisArcTest {
+
+    public static final Vector3d VIEW_SIDE = new Vector3d(1, 0, 0);
+    public static final Vector3d VIEW_ABOVE = new Vector3d(0, 1, 0);
+    private final Vector3d VIEW_TANGENT = new Vector3d(0, 0, 1);
+
     @Before
     public void setUp(){
         List<String> libs = new ArrayList<String>();
@@ -29,6 +34,87 @@ public class AllenMAxisArcTest {
     }
 
     @Test
+    public void AllenMAXisArcGenSimilarArcGeneratesSimilarArcs(){
+        Vector3d view = VIEW_ABOVE;
+
+
+        TestDrawingWindow window = TestDrawingWindow.createDrawerWindow();
+        AllenMAxisArc allenMAxisArc = new AllenMAxisArc();
+        allenMAxisArc.genArc(5,5);
+        allenMAxisArc.transRotMAxis(1, new Point3d(0,0,0), 1, view, 0);
+
+
+
+        for (int i=0; i<50; i++){
+            showNewSimilarArc(window, allenMAxisArc);
+        }
+
+        window.draw(new Drawable() {
+            @Override
+            public void draw() {
+                allenMAxisArc.drawArc(1.0f, 0.0f, 0.0f);
+            }
+        });
+
+        ThreadUtil.sleep(10000);
+
+
+    }
+
+    private void showNewSimilarArc(TestDrawingWindow window, AllenMAxisArc allenMAxisArc) {
+        AllenMAxisArc allenMAxisArc1 = new AllenMAxisArc();
+        allenMAxisArc1.genSimilarArc(allenMAxisArc, 1, 0.7);
+
+
+        window.draw(new Drawable() {
+            @Override
+            public void draw() {
+                allenMAxisArc1.drawArc(1.0f, 1.0f, 0.0f);
+            }
+        });
+    }
+
+    @Test
+    public void MAXisArcGenSimilarArcGenerateSimilarArcs(){
+        Vector3d view = VIEW_ABOVE;
+
+        TestDrawingWindow window = TestDrawingWindow.createDrawerWindow();
+        MAxisArc mAxisArc = new MAxisArc();
+        mAxisArc.genArc(5,5);
+        mAxisArc.transRotMAxis(1, new Point3d(0,0,0), 1, view, 0);
+
+
+
+        for (int i=0; i<50; i++){
+            showNewSimilarArc(window, mAxisArc);
+        }
+
+        window.draw(new Drawable() {
+            @Override
+            public void draw() {
+                mAxisArc.drawArc(1.0f, 0.0f, 0.0f);
+            }
+        });
+
+        ThreadUtil.sleep(10000);
+
+
+    }
+
+    private void showNewSimilarArc(TestDrawingWindow window, MAxisArc mAxisArc) {
+        MAxisArc mAxisArc1 = new MAxisArc();
+        mAxisArc1.genSimilarArc(mAxisArc, 1, 0.7);
+
+
+        window.draw(new Drawable() {
+            @Override
+            public void draw() {
+                mAxisArc1.drawArc(1.0f, 1.0f, 0.0f);
+            }
+        });
+    }
+
+    @Test
     /**
      * Have to verify this test by eye, as it's verified by looking at the lines that are drawn. It should draw a fan
      * of lines, each with uniform spacing. Because we are incrementing the devAngle, if the absolute
@@ -39,7 +125,7 @@ public class AllenMAxisArcTest {
         //AllenMAxis devAngle=0
         AllenMAxisArc allenMAxisArc = new AllenMAxisArc();
         allenMAxisArc.genArc(5,5);
-        allenMAxisArc.transRotMAxis(1, new Point3d(0,0,0), 1, new Vector3d(0,0,1), 0);
+        allenMAxisArc.transRotMAxis(1, new Point3d(0,0,0), 1, VIEW_TANGENT, 0);
         ThreadUtil.sleep(1000);
         window.draw(new Drawable() {
             @Override
@@ -57,7 +143,7 @@ public class AllenMAxisArcTest {
         //AllenMAxis devAngle=pi
         AllenMAxisArc allenMAxisArc1 = new AllenMAxisArc();
         allenMAxisArc1.copyFrom(allenMAxisArc);
-        allenMAxisArc1.transRotMAxis(1, new Point3d(0,0,0), 1, new Vector3d(0,0,1), Math.PI/2);
+        allenMAxisArc1.transRotMAxis(1, new Point3d(0,0,0), 1, VIEW_TANGENT, Math.PI/2);
         double deviateAngle = 0;
         for (int i=0; i<100; i++){
             drawArcAtAngle(window, allenMAxisArc1, deviateAngle, 1.0f, 1.0f, 0.0f);
@@ -77,7 +163,7 @@ public class AllenMAxisArcTest {
         //MAxisArc devAngle=0
         MAxisArc mAxisArc = new MAxisArc();
         mAxisArc.genArc(5,5);
-        mAxisArc.transRotMAxis(1, new Point3d(0,0,0), 1, new Vector3d(0,0,1), 0);
+        mAxisArc.transRotMAxis(1, new Point3d(0,0,0), 1, VIEW_TANGENT, 0);
         ThreadUtil.sleep(1000);
         window.draw(new Drawable() {
             @Override
@@ -111,7 +197,7 @@ public class AllenMAxisArcTest {
         //MAxisArc devAngle=0
         MAxisArc mAxisArc = new MAxisArc();
         mAxisArc.genArc(5,5);
-        mAxisArc.transRotMAxis(1, new Point3d(0,0,0), 1, new Vector3d(0,0,1), 0);
+        mAxisArc.transRotMAxis(1, new Point3d(0,0,0), 1, VIEW_TANGENT, 0);
         ThreadUtil.sleep(1000);
         window.draw(new Drawable() {
             @Override
@@ -136,7 +222,7 @@ public class AllenMAxisArcTest {
     }
 
     private void drawArcAtAngle(TestDrawingWindow window, MAxisArc mAxisArc1, double deviateAngle, final float red, final float green, final float blue) {
-        mAxisArc1.transRotMAxis(1, new Point3d(0,0,0), 1, new Vector3d(0,0,1), deviateAngle);
+        mAxisArc1.transRotMAxis(1, new Point3d(0,0,0), 1, VIEW_TANGENT, deviateAngle);
         window.draw(new Drawable() {
             @Override
             public void draw() {
@@ -146,7 +232,7 @@ public class AllenMAxisArcTest {
     }
 
     private void drawArcAtAngle(TestDrawingWindow window, AllenMAxisArc allenMAxisArc1, double deviateAngle, float red, float green, float blue) {
-        allenMAxisArc1.transRotMAxis(1, new Point3d(0,0,0), 1, new Vector3d(0,0,1), deviateAngle);
+        allenMAxisArc1.transRotMAxis(1, new Point3d(0,0,0), 1, VIEW_TANGENT, deviateAngle);
         window.draw(new Drawable() {
             @Override
             public void draw() {
