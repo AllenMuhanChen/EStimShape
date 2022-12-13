31d0d5ee5da1eb55bd58701b2ae8058af6882ed3
Allen Chen <allenmuhanchen@gmail.com>
2022-12-13 16:33:32 -0500

    New Connection

--- EStimShapeAnalysis/src/compile/field_getters.py
+++ EStimShapeAnalysis/src/compile/field_getters.py
@@ -1,18 +1,17 @@
 import xmltodict
 from mysql.connector import CMySQLConnection
 
+from src.util.connection import Connection
 from src.util.time_util import When
 
 
-def get_stim_spec_id(conn: CMySQLConnection, when: When):
-    my_cursor = conn.cursor()
-    my_cursor.execute(
+def get_stim_spec_id(conn: Connection, when: When):
+    conn.execute(
         "SELECT msg from BehMsg WHERE "
         "msg LIKE '%TrialMessage%' AND "
         "tstamp >= %s AND tstamp <= %s",
         params=(when.start, when.stop))
-
-    trial_msg_xml = fetch_one(my_cursor)
+    trial_msg_xml = conn.fetch_one()
     trial_msg_dict = xmltodict.parse(trial_msg_xml)
     return int(trial_msg_dict['TrialMessage']['stimSpecId'])
 
@@ -23,15 +22,13 @@ StimSpec """
 
 def get_stim_spec_data(conn: CMySQLConnection, when: When):
     stim_spec_id = get_stim_spec_id(conn, when)
-    my_cursor = conn.cursor()
-    my_cursor.execute("SELECT data from StimSpec WHERE "
+    conn.execute("SELECT data from StimSpec WHERE "
                       "id = %s",
                       params=(stim_spec_id,))
 
-    stim_spec_data_xml = fetch_one(my_cursor)
+    stim_spec_data_xml = conn.fetch_one()
     stim_spec_data_dict = xmltodict.parse(stim_spec_data_xml)
     return stim_spec_data_dict
 
 
-def fetch_one(my_cursor):
-    return "".join(my_cursor.fetchall()[0])
+
--- EStimShapeAnalysis/tests/compile/test_new_compile.py
+++ EStimShapeAnalysis/tests/compile/test_new_compile.py
@@ -3,17 +3,19 @@ import unittest
 import mysql
 
 from src.compile.field_getters import get_stim_spec_data, get_stim_spec_id
+from src.util.connection import Connection
 from src.util.time_util import When
 
 
 class MyTestCase(unittest.TestCase):
     fields = [get_stim_spec_data, get_stim_spec_id]
-    conn = mysql.connector.connect(
-        host="172.30.6.80",
-        user="xper_rw",
-        password="up2nite",
-        database="allen_estimshape_test_220729"
-    )
+    # conn = mysql.connector.connect(
+    #     host="172.30.6.80",
+    #     user="xper_rw",
+    #     password="up2nite",
+    #     database="allen_estimshape_test_220729"
+    # )
+    conn = Connection("allen_estimshape_test_220729")
     PSYCHOMETRIC = When(1659208461019365, 1659208471171128)
     RANDOM_CORRECT = When(1659126605490042, 1659126611270426)
 
