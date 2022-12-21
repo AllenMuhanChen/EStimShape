5592d6c9e46f5b62906f5ac2a8a8b17cb138fcd0
Allen Chen <allenmuhanchen@gmail.com>
2022-12-21 12:38:41 -0500

    Python matchstick fields for new data struct

--- EStimShapeAnalysis/tests/compile/test_matchstick_fields.py
+++ EStimShapeAnalysis/tests/compile/test_matchstick_fields.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import xmltodict
 
-from src.compile.matchstick_fields import MatchStickField, ShaftField
+from src.compile.matchstick_fields import MatchStickField, ShaftField, TerminationField, JunctionField
 from src.util.time_util import When
 
 
@@ -11,11 +11,38 @@ class Test(TestCase):
     def test_match_stick_field(self):
         field = MatchStickField(TestMStickSource())
         mstick_spec = field.get(None)
-        assert(list(mstick_spec.keys())[0] == "AllenMStickSpec")
+        assert(list(mstick_spec.keys())[0] == "AllenMStickData")
 
     def test_shaft_field(self):
         field = ShaftField(TestMStickSource())
-        print(field.get(None))
+        for field in field.get(None):
+            data_fields = field.keys()
+            assert(data_fields.__contains__('angularPosition'))
+            assert(data_fields.__contains__('radialPosition'))
+            assert(data_fields.__contains__('orientation'))
+            assert(data_fields.__contains__('radius'))
+            assert(data_fields.__contains__('length'))
+            assert(data_fields.__contains__('curvature'))
+
+    def test_termination_field(self):
+        field = TerminationField(TestMStickSource())
+        for field in field.get(None):
+            data_fields = field.keys()
+            assert(data_fields.__contains__('angularPosition'))
+            assert (data_fields.__contains__('radialPosition'))
+            assert (data_fields.__contains__('direction'))
+            assert (data_fields.__contains__('radius'))
+
+    def test_junction_field(self):
+        field = JunctionField(TestMStickSource())
+        for field in field.get(None):
+            print(field)
+            data_fields = field.keys()
+            print(data_fields)
+            assert(data_fields.__contains__('angularPosition'))
+            assert (data_fields.__contains__('radialPosition'))
+            assert (data_fields.__contains__('angleBisectorDirection'))
+            assert (data_fields.__contains__('radius'))
 
     def test_comp_txt(self):
         comp_file = open("/home/r2_allen/Documents/EStimShape/dev_221110/specs/1670009332620530_comp.txt")
@@ -24,10 +51,11 @@ class Test(TestCase):
         print(comp)
 
 
+
 class TestMStickSource:
 
     def get(self, when:When):
-        xml_file = open("/home/r2_allen/Documents/EStimShape/dev_221110/specs/1670009332620530_spec.xml")
+        xml_file = open("/home/r2_allen/git/EStimShape/xper-train/xper-allen/test/test-resources/testBin/AllenMStickDataTest_testFile_spec.xml")
         spec = xml_file.read()
         xml_file.close()
         return spec
