8bbaf3cca25058bbbe155071903a0fb84d201b91
Allen Chen <allenmuhanchen@gmail.com>
2023-01-12 16:33:19 -0500

    Attempts to plot better RWA
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- EStimShapeAnalysis/src/mock/mock_rwa_analysis.py
+++ EStimShapeAnalysis/src/mock/mock_rwa_analysis.py
@@ -6,8 +6,7 @@ import jsonpickle as jsonpickle
 import numpy as np
 import pandas as pd
 
-
-from src.analysis.rwa import rwa, Binner
+from src.analysis.rwa import rwa, Binner, LabelledMatrix
 from src.compile.classic_database_fields import StimSpecDataField, StimSpecIdField, GaTypeField, GaLineageField
 from src.compile.matchstick_fields import ShaftField, TerminationField, JunctionField
 from src.compile.trial_field import FieldList, DatabaseField, get_data_from_trials
@@ -17,17 +16,28 @@ from src.util.connection import Connection
 from src.util.time_util import When
 
 
+def compute_rwa_from_lineages(data, ga_type, binner_for_fields):
+    data = data.loc[data['GaType'] == ga_type]
+    rwas = []
+    for (lineage, lineage_data) in data.groupby("Lineage"):
+        rwas.append(rwa(lineage_data["Shaft"], lineage_data["Response-1"], binner_for_fields))
+    print("Multiplying Lineage RWAs")
+    rwas_labelled_matrices = [next(r) for r in rwas]
+    rwa_prod = np.prod(np.array([rwa_labelled_matrix.matrix for rwa_labelled_matrix in rwas_labelled_matrices]), axis=0)
+    return rwas_labelled_matrices[0].copy_labels(rwa_prod)
+
+
 def main():
     # PARAMETERS
     conn = Connection("allen_estimshape_dev_221110")
-    bin_size = 10
+    num_bins = 10
     binner_for_shaft_fields = {
-        "theta": Binner(-pi, pi, bin_size),
-        "phi": Binner(0, pi, bin_size),
-        "radialPosition": Binner(0, 100, bin_size),
-        "length": Binner(0, 200, bin_size),
-        "curvature": Binner(0, 1, bin_size),
-        "radius": Binner(0, 20, bin_size),
+        "theta": Binner(-pi, pi, num_bins),
+        "phi": Binner(0, pi, num_bins),
+        "radialPosition": Binner(0, 100, num_bins),
+        "length": Binner(0, 100, num_bins),
+        "curvature": Binner(0, 1, num_bins),
+        "radius": Binner(0, 20, num_bins),
     }
 
     # PIPELINE
@@ -35,27 +45,13 @@ def main():
     data = compile_data(conn, trial_tstamps)
     data = condition_spherical_angles(data)
     data = condition_for_inside_bins(data, binner_for_shaft_fields)
-    response_weighted_average = rwa(data["Shaft"], data["Response-1"], binner_for_shaft_fields)
+    response_weighted_average = compute_rwa_from_lineages(data, "3D", binner_for_shaft_fields)
+    # response_weighted_average = rwa(data["Shaft"], data["Response-1"], binner_for_shaft_fields)
 
     # SAVE
     filename = "/home/r2_allen/Documents/EStimShape/dev_221110/rwa/test_rwa.json"
     with open(filename, "w") as file:
         file.write(jsonpickle.encode(response_weighted_average))
-
-
-class MockResponseField(StimSpecIdField):
-
-    def __init__(self, conn: Connection, channel: int, name: str = None):
-        super().__init__(conn, name=name)
-        self.channel = channel
-
-    def get(self, when: When) -> float:
-        stim_spec_id = StimSpecIdField.get(self, when)
-        self.conn.execute("SELECT memo FROM ExpLog WHERE tstamp = %s", [stim_spec_id])
-        responses_for_channels = self.conn.fetch_one()
-        responses_for_channels = ast.literal_eval(responses_for_channels)
-        response = responses_for_channels.get(self.channel)
-        return response
 
 
 def compile_data(conn: Connection, trial_tstamps: list[When]) -> pd.DataFrame:
@@ -72,6 +68,21 @@ def compile_data(conn: Connection, trial_tstamps: list[When]) -> pd.DataFrame:
 
     data = get_data_from_trials(fields, trial_tstamps)
     return data
+
+
+class MockResponseField(StimSpecIdField):
+
+    def __init__(self, conn: Connection, channel: int, name: str = None):
+        super().__init__(conn, name=name)
+        self.channel = channel
+
+    def get(self, when: When) -> float:
+        stim_spec_id = StimSpecIdField.get(self, when)
+        self.conn.execute("SELECT memo FROM ExpLog WHERE tstamp = %s", [stim_spec_id])
+        responses_for_channels = self.conn.fetch_one()
+        responses_for_channels = ast.literal_eval(responses_for_channels)
+        response = responses_for_channels.get(self.channel)
+        return response
 
 
 def condition_spherical_angles(data):
@@ -120,7 +131,8 @@ def condition_for_inside_bins(data: pd.DataFrame, binner_for_fields: dict[str, B
         if column_data.dtype == object:
             for stim_index, stim_data in enumerate(column_data.array):
                 is_field_outside_range = False
-                if recursively_check_condition_for_subdictionaries(stim_data, check_if_outside_binrange, is_field_outside_range, binner_for_fields):
+                if recursively_check_condition_for_subdictionaries(stim_data, check_if_outside_binrange,
+                                                                   is_field_outside_range, binner_for_fields):
                     row_indices_to_remove.append(stim_index)
     data = data.drop(labels=row_indices_to_remove, axis=0, inplace=False)
     return data
