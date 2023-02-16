660608bc0e1304914f30e705cd3bd4fc739b8491
Allen Chen <allenmuhanchen@gmail.com>
2023-02-16 14:02:57 -0500

    to xml and from xml for GABranch
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/test/unit-tests/org/xper/allen/ga/GABranchTest.java
+++ xper-train/xper-allen/test/unit-tests/org/xper/allen/ga/GABranchTest.java
@@ -1,10 +1,14 @@
 package org.xper.allen.ga;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Consumer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GABranchTest {
 
@@ -16,12 +20,12 @@ public class GABranchTest {
     }
 
     @Test
-    public void addsChildToCurrentBranch() throws Exception {
+    public void addsChildToCurrentBranch(){
         GABranch child = new GABranch(2L);
         founder.addChild(child);
 
         System.out.println(founder);
-        Assert.assertTrue(founder.getChildren().contains(child));
+        assertTrue(founder.getChildren().contains(child));
     }
 
     @Test
@@ -35,12 +39,12 @@ public class GABranchTest {
         founder.addChildTo(2L, testBranch);
 
         System.out.println(founder);
-        Assert.assertTrue(child1.getChildren().contains(testBranch));
-        Assert.assertTrue(child2.getChildren().isEmpty());
+        assertTrue(child1.getChildren().contains(testBranch));
+        assertTrue(child2.getChildren().isEmpty());
     }
 
     @Test
-    public void canReadAllIds(){
+    public void can_parse_tree(){
         GABranch child1 = new GABranch(2L);
         GABranch child2 = new GABranch(3L);
         founder.addChild(child1);
@@ -48,13 +52,36 @@ public class GABranchTest {
 
         GABranch testBranch = new GABranch(4L);
         founder.addChildTo(2L, testBranch);
-
+        List<Long> childrenIds = new LinkedList<>();
         founder.forEach(new Consumer<GABranch>() {
             @Override
             public void accept(GABranch branch) {
                 System.out.println(branch.getBranchId());
+                childrenIds.add(branch.getBranchId());
             }
         });
+
+        assertTrue(childrenIds.contains(1L));
+        assertTrue(childrenIds.contains(2L));
+        assertTrue(childrenIds.contains(3L));
+        assertTrue(childrenIds.contains(4L));
+    }
+
+    @Test
+    public void to_xml_and_back(){
+        GABranch child1 = new GABranch(2L);
+        GABranch child2 = new GABranch(3L);
+        founder.addChild(child1);
+        founder.addChild(child2);
+        GABranch child3 = new GABranch(4L);
+        founder.addChildTo(2L, child3);
+
+        String xml = founder.toXml();
+        System.out.println(xml);
+        GABranch fromXML = GABranch.fromXml(xml);
+        System.out.println(fromXML);
+
+        assertEquals(founder, fromXML);
     }
 
 
