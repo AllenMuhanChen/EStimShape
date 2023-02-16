fdec82d73f66f44b802e3d47667c1a410f7e162e
Allen Chen <allenmuhanchen@gmail.com>
2023-02-16 14:02:57 -0500

    to xml and from xml for GABranch
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/src/org/xper/allen/ga/GABranch.java
+++ xper-train/xper-allen/src/org/xper/allen/ga/GABranch.java
@@ -1,13 +1,20 @@
 package org.xper.allen.ga;
 
+import com.thoughtworks.xstream.XStream;
+
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class GABranch {
 
     private Collection<GABranch> children;
     private Long branchId;
+
+
+    public GABranch() {
+    }
 
     public GABranch(Long branchId, Collection<GABranch> children){
         this.branchId = branchId;
@@ -17,6 +24,25 @@ public class GABranch {
     public GABranch(Long branchId){
         this.branchId = branchId;
         this.children = new LinkedList<>();
+    }
+
+    static XStream s;
+
+    static{
+        s = new XStream();
+        s.alias("GABranch", GABranch.class);
+    }
+
+    public String toXml(){
+        return GABranch.toXml(this);
+    }
+
+    public static String toXml(GABranch gaBranch){
+        return s.toXML(gaBranch);
+    }
+
+    public static GABranch fromXml(String xml){
+        return (GABranch) s.fromXML(xml);
     }
 
     public void addChild(GABranch child){
@@ -64,5 +90,18 @@ public class GABranch {
               s += "-"+child.toString();
          }
            return s;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GABranch gaBranch = (GABranch) o;
+        return getChildren().equals(gaBranch.getChildren()) && getBranchId().equals(gaBranch.getBranchId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getChildren(), getBranchId());
     }
 }
