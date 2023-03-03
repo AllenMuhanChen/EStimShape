c43c64c08d2a3cd48971b7a53d10f80c2095c3fc
Allen Chen <allenmuhanchen@gmail.com>
2023-03-03 14:07:49 -0500

    Basic framework for testing mutations, i.e loading a test stick, drawing results
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

---
+++ xper-train/xper-allen/src/org/xper/allen/drawing/composition/ga/GrowingMatchStick.java
@@ -0,0 +1,12 @@
+package org.xper.allen.drawing.composition.ga;
+
+import org.xper.allen.drawing.composition.AllenMatchStick;
+
+public class GrowingMatchStick extends AllenMatchStick {
+
+    @Override
+    public boolean mutate(int debugParam) {
+
+        return false;
+    }
+}
