695742dcfffed994abd1e6bd8e2743b3695c0113
Allen Chen <allenmuhanchen@gmail.com>
2023-03-14 11:07:28 -0400

    Max Value Linaege Score and associated Test
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/src/org/xper/allen/ga/regimescore/MaxValueLineageScore.java
+++ xper-train/xper-allen/src/org/xper/allen/ga/regimescore/MaxValueLineageScore.java
@@ -7,7 +7,9 @@ import org.xper.allen.ga.SpikeRateSource;
 import org.xper.allen.util.MultiGaDbUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MaxValueLineageScore implements LineageScoreSource {
 
@@ -26,17 +28,23 @@ public class MaxValueLineageScore implements LineageScoreSource {
         List<Long> stimIds = dbUtil.readStimIdsFromLineageAndType(lineageId, "RAND");
 
         // find spike rates of all stim_ids
+        Map<Long, Double> averageSpikeRateForStimIds = new HashMap<Long, Double>();
         for (Long stimId : stimIds) {
             List<Double> spikeRates = spikeRateSource.getSpikeRates(stimId);
             Double averageSpikeRate = getAverageSpikeRate(spikeRates);
-            Double score = getScore(spikeRate, threshold);
+            averageSpikeRateForStimIds.put(stimId, averageSpikeRate);
         }
 
         // return max spike rate
+        Double max = averageSpikeRateForStimIds.values().stream().max(Double::compare).get();
 
-        // return (threshold-max)/max
-
-        return null;
+        // normalize by threshold such that when max == threshold, score == 1
+        Double threshold = thresholdSource.getThreshold();
+        Double score = max / threshold;
+        if (score > 1.0) {
+            score = 1.0;
+        }
+        return score;
     }
 
     private Double getAverageSpikeRate(List<Double> spikeRates) {
