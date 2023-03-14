b74773f54dfa2505d4f3cfd788397587b05f9649
Allen Chen <allenmuhanchen@gmail.com>
2023-03-14 11:07:28 -0400

    Max Value Linaege Score and associated Test
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/test/unit-tests/org/xper/allen/ga/regimescore/MaxValueLineageScoreTest.java
+++ xper-train/xper-allen/test/unit-tests/org/xper/allen/ga/regimescore/MaxValueLineageScoreTest.java
@@ -2,6 +2,7 @@ package org.xper.allen.ga.regimescore;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.xper.allen.ga.SpikeRateSource;
 import org.xper.allen.util.MultiGaDbUtil;
 
 import java.util.ArrayList;
@@ -18,23 +19,61 @@ public class MaxValueLineageScoreTest {
         source = new MaxValueLineageScore();
 
         source.setDbUtil(new MockDbUtil());
+        source.setSpikeRateSource(new MockSpikeRateSource());
+        source.setThresholdSource(new ThresholdSource() {
+            @Override
+            public Double getThreshold() {
+                return 2.0;
+            }
+        });
     }
 
     @Test
-    public void getLineageScore() {
-        Double score = source.getLineageScore(1L);
+    public void under_threshold_returns_right_value(){
+        source.setDbUtil(new MockDbUtil() {
+            @Override
+            public List<Long> readStimIdsFromLineageAndType(Long lineageId, String type) {
+                List<Long> stimIds = new ArrayList<Long>();
+                stimIds.add(1L);
+                return stimIds;
+            }
+        });
 
+        Double score = source.getLineageScore(1L);
+        assertEquals(0.5, score, 0.001);
+    }
+
+    @Test
+    public void over_threshold_returns_1() {
+        source.setDbUtil(new MockDbUtil() {
+            @Override
+            public List<Long> readStimIdsFromLineageAndType(Long lineageId, String type) {
+                List<Long> stimIds = new ArrayList<Long>();
+                stimIds.add(1L);
+                stimIds.add(2L);
+                stimIds.add(3L);
+                return stimIds;
+            }
+        });
+        Double score = source.getLineageScore(1L);
+        assertEquals(1, score, 0.001);
 
     }
 
     private static class MockDbUtil extends MultiGaDbUtil {
+
+    }
+
+    private static class MockSpikeRateSource implements SpikeRateSource {
         @Override
-        public List<Long> readStimIdsFromLineageAndType(Long lineageId, String type) {
-            List<Long> stimIds = new ArrayList<Long>();
-            stimIds.add(1L);
-            stimIds.add(2L);
-            stimIds.add(3L);
-            return stimIds;
+        public List<Double> getSpikeRates(Long taskId) {
+            List<Double> spikeRates = new ArrayList<Double>();
+            spikeRates.add(Double.valueOf(taskId));
+            return spikeRates;
         }
     }
+
+
+
+
 }
