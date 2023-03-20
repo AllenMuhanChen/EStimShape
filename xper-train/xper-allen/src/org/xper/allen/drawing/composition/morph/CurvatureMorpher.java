cd4836b2bdd610f6308e46a0c561155c31e8f2d7
Allen Chen <allenmuhanchen@gmail.com>
2023-03-20 16:02:56 -0400

    CurvatureMorpher, not comprehensive testing
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

---
+++ xper-train/xper-allen/src/org/xper/allen/drawing/composition/morph/CurvatureMorpher.java
@@ -0,0 +1,66 @@
+package org.xper.allen.drawing.composition.morph;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class CurvatureMorpher {
+    public final static double RADIUS_VIEW = 5.0;
+    public final static double LOW_CURVATURE_UPPER = 1 / (6 * RADIUS_VIEW);
+    public static final double MEDIUM_CURVATURE_LOWER = LOW_CURVATURE_UPPER;
+    public static final double MEDIUM_CURVATURE_HIGHER = 1 / (0.6 * RADIUS_VIEW);
+    public static final double HIGH_CURVATURE_LOWER = MEDIUM_CURVATURE_HIGHER;
+    public static final double HIGH_CURVATURE_UPPER = 1 / (0.2 * RADIUS_VIEW);
+
+    public Double morphCurvature(Double oldCurvature, Double curvatureMagnitude) {
+        // Convert oldCurvature to a normalized curvature value
+        double oldNormalizedCurvature;
+        if (oldCurvature < LOW_CURVATURE_UPPER){
+            oldNormalizedCurvature = 0;
+        }
+        else if (oldCurvature < MEDIUM_CURVATURE_HIGHER) {
+            double normalizedMediumCurvature = (oldCurvature - MEDIUM_CURVATURE_LOWER) / (MEDIUM_CURVATURE_HIGHER - MEDIUM_CURVATURE_LOWER);
+            oldNormalizedCurvature = 1.0/3.0 + normalizedMediumCurvature/3.0;
+        }
+        else if (oldCurvature <= HIGH_CURVATURE_UPPER) {
+            double normalizedHighCurvature = (oldCurvature - HIGH_CURVATURE_LOWER) / (HIGH_CURVATURE_UPPER - HIGH_CURVATURE_LOWER);
+            oldNormalizedCurvature = 2.0/3.0 + normalizedHighCurvature / 3.0;
+        }
+        else {
+            throw new RuntimeException("Curvature value out of range: " + oldCurvature);
+        }
+
+        // Use curvature magnitude to determine the new normalized curvature value
+        double distToMin = Math.abs(oldNormalizedCurvature);
+        double distToMax = Math.abs(1 - oldNormalizedCurvature);
+        double maxDist = Math.max(distToMin, distToMax);
+        double normalizedDistToMin = distToMin / maxDist;
+        double normalizedDistToMax = distToMax / maxDist;
+
+        List<Double> possibleNormalizedCurvatures = new ArrayList<>();
+        if (normalizedDistToMin >= curvatureMagnitude)
+            possibleNormalizedCurvatures.add(oldNormalizedCurvature - curvatureMagnitude*oldNormalizedCurvature);
+        if (normalizedDistToMax >= curvatureMagnitude)
+            possibleNormalizedCurvatures.add(oldNormalizedCurvature + curvatureMagnitude*(1.0-oldNormalizedCurvature));
+        Collections.shuffle(possibleNormalizedCurvatures);
+
+        Double newNormalizedCurvature = possibleNormalizedCurvatures.get(0);
+
+        // Convert the new normalized curvature value to a new curvature value
+        if (newNormalizedCurvature <= 1.0/3.0) {
+            return 1.0/10000;
+        }
+        else if (newNormalizedCurvature <= 2.0/3.0) {
+            double normalizedMediumCurvature = (newNormalizedCurvature - 1.0/3.0) * 3.0;
+            return MEDIUM_CURVATURE_LOWER + normalizedMediumCurvature * (MEDIUM_CURVATURE_HIGHER - MEDIUM_CURVATURE_LOWER);
+        }
+        else if (newNormalizedCurvature <= 1.0) {
+            double normalizedHighCurvature = (newNormalizedCurvature - 2.0/3.0) * 3.0;
+            return HIGH_CURVATURE_LOWER + normalizedHighCurvature * (HIGH_CURVATURE_UPPER - HIGH_CURVATURE_LOWER);
+        }
+        else{
+            throw new RuntimeException("New normalized curvature value out of range: " + newNormalizedCurvature);
+        }
+    }
+}
