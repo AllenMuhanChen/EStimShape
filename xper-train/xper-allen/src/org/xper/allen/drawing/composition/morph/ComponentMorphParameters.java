9802b09b3b121c1da43266eb8494dea9a60a8228
Allen Chen <allenmuhanchen@gmail.com>
2023-03-20 12:49:04 -0400

    OrientationMorpher
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

---
+++ xper-train/xper-allen/src/org/xper/allen/drawing/composition/morph/ComponentMorphParameters.java
@@ -0,0 +1,80 @@
+package org.xper.allen.drawing.composition.morph;
+
+import javax.vecmath.Vector3d;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ComponentMorphParameters {
+
+    public static double MAX;
+    Double magnitude;
+
+    public ComponentMorphParameters(Double magnitude) {
+        this.magnitude = magnitude;
+
+        distributeMagnitude();
+    }
+
+    Vector3d orientation;
+    Double rotation;
+    Double length;
+    Double curvature;
+    RadiusProfile radiusProfile;
+
+    public Vector3d getOrientation(Vector3d oldOrientation){
+        return null;
+    }
+
+    Double orientationMagnitude;
+    Double rotationMagnitude;
+    Double lengthMagnitude;
+    Double curvatureMagnitude;
+    Double radiusProfileMagnitude;
+
+    private void distributeMagnitude() {
+        AtomicReference<Double> orientationMagnitude = new AtomicReference<>(0.0);
+        AtomicReference<Double> rotationMagnitude = new AtomicReference<>(0.0);
+        AtomicReference<Double> lengthMagnitude = new AtomicReference<>(0.0);
+        AtomicReference<Double> curvatureMagnitude = new AtomicReference<>(0.0);
+        AtomicReference<Double> radiusProfileMagnitude = new AtomicReference<>(0.0);
+
+        List<AtomicReference<Double>> magnitudes = new ArrayList<>();
+        magnitudes.add(orientationMagnitude);
+        magnitudes.add(rotationMagnitude);
+        magnitudes.add(lengthMagnitude);
+        magnitudes.add(curvatureMagnitude);
+        magnitudes.add(radiusProfileMagnitude);
+        MAX = 1.0 / magnitudes.size();
+
+        Collections.shuffle(magnitudes);
+
+        Double amountToDistribute = magnitude;
+        for (AtomicReference<Double> magnitude : magnitudes) {
+            double randomMagnitude = Math.random() * MAX;
+            amountToDistribute -= randomMagnitude;
+            if (amountToDistribute > 0) {
+                double normalizedMagnitude = randomMagnitude / MAX;
+                magnitude.set(normalizedMagnitude);
+            }
+            else {
+                magnitude.set(amountToDistribute + randomMagnitude);
+                break;
+            }
+        }
+
+        this.orientationMagnitude = orientationMagnitude.get();
+        this.rotationMagnitude = rotationMagnitude.get();
+        this.lengthMagnitude = lengthMagnitude.get();
+        this.curvatureMagnitude = curvatureMagnitude.get();
+        this.radiusProfileMagnitude = radiusProfileMagnitude.get();
+    }
+
+    public static class RadiusProfile {
+    }
+
+
+
+
+}
