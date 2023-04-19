cacd0b05b5ade2297af2d06ce341bc1645fcb235
Allen Chen <allenmuhanchen@gmail.com>
2023-04-19 16:05:10 -0400

    Fixed bug with AngularCoordinates
    Signed-off-by: Allen Chen <allenmuhanchen@gmail.com>

--- xper-train/xper-allen/src/org/xper/allen/drawing/composition/AngularCoordinates.java
+++ xper-train/xper-allen/src/org/xper/allen/drawing/composition/AngularCoordinates.java
@@ -4,6 +4,9 @@ public class AngularCoordinates {
     public double theta;
     public double phi;
 
+    public AngularCoordinates() {
+    }
+
     public AngularCoordinates(double theta, double phi) {
         this.theta = theta;
         this.phi = phi;
