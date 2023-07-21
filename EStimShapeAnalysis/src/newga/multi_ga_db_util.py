647d30d200bcfe7ede66920958399a147fb751ef
Allen Chen <allenmuhanchen@gmail.com>
2023-07-21 14:45:08 -0400

    Finished but untested (not sure if necessary to test this) ResponseParser

--- EStimShapeAnalysis/src/newga/multi_ga_db_util.py
+++ EStimShapeAnalysis/src/newga/multi_ga_db_util.py
@@ -42,6 +42,50 @@ class MultiGaDbUtil:
             "INSERT IGNORE INTO StimGaInfo (stimId, parentId, gaName, genId, lineageId, stimType) VALUES (%s, %s, %s, %s, %s, %s)",
             (stim_id, parent_id, ga_name, gen_id, lineage_id, stim_type))
 
+    def read_task_done_ids_for_stim_id(self, ga_name: str, stim_id: int):
+        self.conn.execute(
+            "SELECT d.task_id "
+            "FROM TaskDone d "
+            "JOIN TaskToDo t ON d.task_id = t.task_id "
+            "WHERE t.ga_name = %s AND t.stim_id = %s",
+            (ga_name, stim_id))
+
+        rows = self.conn.fetch_all()
+        task_ids = [row[0] for row in rows]
+
+        return task_ids
+
+    def read_stims_with_no_response(self):
+        self.conn.execute(
+            "SELECT s.stim_id "
+            "FROM StimGaInfo s "
+            "LEFT JOIN StimResponses r ON s.stim_id = r.stim_id "
+            "WHERE r.stim_id IS NULL")
+
+        rows = self.conn.fetch_all()
+        stim_ids = [row[0] for row in rows]
+
+        return stim_ids
+
+    def add_stim_responses(self, stim_id: int, task_ids: list, responses: list):
+        if len(task_ids) != len(responses):
+            raise ValueError("The length of task_ids and spike_counts must be the same")
+
+        for task_id, response in zip(task_ids, responses):
+            self.conn.execute(
+                "INSERT INTO StimResponses (stim_id, task_id, response) VALUES (%s, %s, %s)",
+                (stim_id, task_id, response))
+
+    def add_stim_channels(self, stim_id: int, channels: list):
+        for channel in channels:
+            self.conn.execute(
+                "INSERT INTO StimResponseChannels (stim_id, channel) VALUES (%s, %s)",
+                (stim_id, channel))
+
+    def update_stim_ga_info_response(self, stim_id: int, response: float):
+        self.conn.execute(
+            "UPDATE StimGaInfo SET response = %s WHERE stim_id = %s",
+            (response, stim_id))
 
 class MultiGaGenerationInfo:
     def __init__(self, gen_id_for_ga=None):
