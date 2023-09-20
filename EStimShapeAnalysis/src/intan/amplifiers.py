a78c82fd96075f82d64f1d88b33d53aeb2fe25fd
Allen Chen <allenmuhanchen@gmail.com>
2023-09-20 11:29:45 -0400

    amplifiers.py now returns a dict between Channels and the array of voltages

--- EStimShapeAnalysis/src/intan/amplifiers.py
+++ EStimShapeAnalysis/src/intan/amplifiers.py
@@ -1,18 +1,22 @@
 import numpy as np
 import os
 
+from intan.channels import Channel
+
 
 def read_amplifier_data(file_path, amplifier_channels):
     """
     Reads amplifier data from a binary file and converts it to microvolts.
+    Creates a dictionary mapping each native_channel_name to its vector of spikes.
 
     Parameters:
     - file_path: str, path to the amplifier.dat file
-    - amplifier_channels: list, amplifier channel information
+    - amplifier_channels: list of dicts, amplifier channel information
 
     Returns:
-    - v: numpy.ndarray, amplifier data in microvolts
+    - channel_to_data: dict, mapping from native_channel_name to spike data in microvolts
     """
+    # Extract the number of channels
     num_channels = len(amplifier_channels)
 
     # Get file size in bytes
@@ -20,6 +24,9 @@ def read_amplifier_data(file_path, amplifier_channels):
 
     # Calculate the number of samples
     num_samples = fileinfo // (num_channels * 2)  # int16 = 2 bytes
+
+    # Initialize an empty dictionary to store the mapping
+    channel_to_data = {}
 
     # Open the file and read the data
     with open(file_path, 'rb') as fid:
@@ -31,7 +38,12 @@ def read_amplifier_data(file_path, amplifier_channels):
     # Convert to microvolts
     v = v * 0.195
 
-    return v
+    # Populate the dictionary
+    for i, channel_info in enumerate(amplifier_channels):
+        native_channel_name = channel_info.get("native_channel_name", f"Channel_{i}")
+        channel_to_data[Channel(native_channel_name)] = v[i, :]
+
+    return channel_to_data
 
 
 
