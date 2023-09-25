65874e9c1c99dcb3fdf96e8179161091c88a1cad
Allen Chen <allenmuhanchen@gmail.com>
2023-09-25 11:50:30 -0400

    added new mergeinfo.txt that stitch creates

--- EStimShapeAnalysis/src/windowsort/gui.py
+++ EStimShapeAnalysis/src/windowsort/gui.py
@@ -79,7 +79,7 @@ def main():
 
     # Define the data directory here
     date = "2023-09-22"
-    exp_name = "1694529683452000_230912_144921"
+    exp_name = "round3"
     # exp_name = "1694801146439198_230915_140547"
     data_directory = "/run/user/1003/gvfs/smb-share:server=connorhome.local,share=connorhome/Julie/IntanData/Cortana/%s/%s/" % (
     date, exp_name)
