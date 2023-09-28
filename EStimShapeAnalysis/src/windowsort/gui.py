8393c892470f0d30485748a35b421367457cf359
Allen Chen <allenmuhanchen@gmail.com>
2023-09-28 10:29:31 -0400

    basic operational spike snapshot

--- EStimShapeAnalysis/src/windowsort/gui.py
+++ EStimShapeAnalysis/src/windowsort/gui.py
@@ -4,6 +4,7 @@ import os
 
 from windowsort.datahandler import DataImporter, DataExporter
 from windowsort.drift import DriftSpikePlot
+from windowsort.snapshot import SnapshotPlot
 from windowsort.timeampwindow import SortSpikePlot
 from windowsort.units import SortPanel
 from windowsort.voltage import VoltageTimePlot, TimeScrubber, ChannelSelectionPanel, ThresholdControlPanel
@@ -31,47 +32,53 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
 
         # Two Columns
-        threshold_layout = QVBoxLayout()
-        spike_plot_layout = QVBoxLayout()
-        spike_sort_layout = QVBoxLayout()
+        threshold_column = QVBoxLayout()
+        spike_plot_column = QVBoxLayout()
+        spike_sort_column = QVBoxLayout()
 
         # Voltage Time Plot
         self.voltage_time_plot = VoltageTimePlot(self.data_handler)
-        threshold_layout.addWidget(self.voltage_time_plot)
+        threshold_column.addWidget(self.voltage_time_plot)
         self.time_scrubber = TimeScrubber(self.voltage_time_plot)
-        threshold_layout.addWidget(self.time_scrubber)
+        threshold_column.addWidget(self.time_scrubber)
         self.threshold_control_panel = ThresholdControlPanel(self.voltage_time_plot)
-        threshold_layout.addWidget(self.threshold_control_panel)
+        threshold_column.addWidget(self.threshold_control_panel)
 
         # Spike Plot
         default_max_spikes = 50
         self.spike_plot = DriftSpikePlot(self.data_handler, self.data_exporter, default_max_spikes=default_max_spikes)
-        spike_plot_layout.addWidget(self.spike_plot)
+        spike_plot_column.addWidget(self.spike_plot)
         self.voltage_time_plot.spike_plot = self.spike_plot
         self.spike_scrubber = SpikeScrubber(self.spike_plot, default_max_spikes=default_max_spikes)
-        spike_plot_layout.addWidget(self.spike_scrubber)
+        spike_plot_column.addWidget(self.spike_scrubber)
         self.spike_plot.spike_scrubber = self.spike_scrubber
+
 
         # Exporting
         self.export_panel = ExportPanel(self.data_exporter)
-        threshold_layout.addWidget(self.export_panel)
-        threshold_layout.addWidget(self.export_panel)
+        threshold_column.addWidget(self.export_panel)
+        threshold_column.addWidget(self.export_panel)
 
         # Channel Selection
         self.channel_selection_pannel = ChannelSelectionPanel(self.voltage_time_plot, self.spike_plot)
-        threshold_layout.insertWidget(0, self.channel_selection_pannel)  # Inserts at the top of the layout
+        threshold_column.insertWidget(0, self.channel_selection_pannel)  # Inserts at the top of the layout
 
-        # Logical Rules
-        spike_sort_panel = SortPanel(self.spike_plot, self.data_exporter, self.voltage_time_plot)
-        spike_sort_layout.insertWidget(0, spike_sort_panel)
-        self.spike_plot.set_sort_panel(spike_sort_panel)
-        self.channel_selection_pannel.sort_panel = spike_sort_panel
+        # Sort Panel
+        self.sort_panel = SortPanel(self.spike_plot, self.data_exporter, self.voltage_time_plot)
+        spike_sort_column.insertWidget(0, self.sort_panel)
+        self.spike_plot.set_sort_panel(self.sort_panel)
+        self.channel_selection_pannel.sort_panel = self.sort_panel
         # Add more Time-Amp related widgets to spike_sort_layout if needed
 
+        # Snapshot Plot
+        self.snapshot_plot = SnapshotPlot(self.data_handler, self.data_exporter, self.sort_panel)
+        self.snapshot_plot.setMaximumHeight(500)
+        spike_plot_column.addWidget(self.snapshot_plot)
+
         # Add the second column layout to the main layout
-        main_layout.addLayout(threshold_layout)
-        main_layout.addLayout(spike_plot_layout)
-        main_layout.addLayout(spike_sort_layout)
+        main_layout.addLayout(threshold_column)
+        main_layout.addLayout(spike_plot_column)
+        main_layout.addLayout(spike_sort_column)
 
         central_widget.setLayout(main_layout)
 
@@ -83,10 +90,10 @@ def main():
 
 
     # Define the data directory here
-    date = "2023-09-26"
-    exp_name = "230926_round2"
-    # date = "2023-09-12"
-    # exp_name = "1694529683452000_230912_144921"
+    # date = "2023-09-26"
+    # exp_name = "230926_round2"
+    date = "2023-09-12"
+    exp_name = "1694529683452000_230912_144921"
     data_directory = "/run/user/1003/gvfs/smb-share:server=connorhome.local,share=connorhome/Julie/IntanData/Cortana/%s/%s/" % (
     date, exp_name)
 
