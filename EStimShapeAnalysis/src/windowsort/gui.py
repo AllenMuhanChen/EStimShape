dabf5018de994dbcbe8b9f21ec00c30dc5b2f204
Allen Chen <allenmuhanchen@gmail.com>
2023-10-09 15:54:52 -0400

    analysis

--- EStimShapeAnalysis/src/windowsort/gui.py
+++ EStimShapeAnalysis/src/windowsort/gui.py
@@ -91,8 +91,8 @@ def main():
 
 
     # Define the data directory here
-    date = "2023-09-26"
-    exp_name = "230926_round2"
+    date = "2023-09-29"
+    exp_name = "230929_round2"
     # date = "2023-09-12"
     # exp_name = "1694529683452000_230912_144921"
     data_directory = "/home/r2_allen/Documents/JulieIntanData/Cortana/%s/%s/" % (
