6bac86bf060c5a2215980512c6e2c1b4fa1e22a1
Allen Chen <allenmuhanchen@gmail.com>
2024-01-08 11:30:08 -0500

    added new cached_fields.py which handles caching values fetched into a database table for analysis and speed up of getting

--- EStimShapeAnalysis/src/analysis/nafc/nafc_database_fields.py
+++ EStimShapeAnalysis/src/analysis/nafc/nafc_database_fields.py
@@ -1,18 +1,23 @@
 import xmltodict
 
 from clat.compile.trial.trial_field import DatabaseField
+from clat.compile.trial.cached_fields import CachedDatabaseField
 from clat.util.connection import Connection
 from clat.util.time_util import When
 
 
-class StimSpecDataField(DatabaseField):
+class StimSpecDataField(CachedDatabaseField):
+
+    def __init__(self, conn: Connection, name: str = None, is_cache_enabled: bool = True, is_store_cache_enabled: bool = True):
+        super().__init__(conn, is_cache_enabled, is_store_cache_enabled, name)
+
     def get(self, when: When):
         return get_stim_spec_data(self.conn, when)
 
 
-class IsCorrectField(DatabaseField):
+class IsCorrectField(CachedDatabaseField):
     def __init__(self, conn: Connection, name="IsCorrect"):
-        super().__init__(conn, name)
+        super().__init__(conn, name=name)
 
     def get(self, when: When):
         # SQL to check for the presence of "ChoiceSelectionCorrect" or "ChoiceSelectionIncorrect" in the specified time frame.
@@ -38,21 +43,23 @@ class IsCorrectField(DatabaseField):
         else:
             return "No Data"
 
+
 class NoiseChanceField(StimSpecDataField):
     def __init__(self, conn: Connection, name="NoiseChance"):
         self.name = "NoiseChance"
-        super().__init__(conn, name)
+        super().__init__(conn, name=name)
 
     def get(self, when: When):
-        stim_spec_data = super().get(when)
+        stim_spec_data = super().get_and_cache(when)
         noiseChance = stim_spec_data[next(iter(stim_spec_data))]["noiseChance"]
         noiseChance = float(noiseChance)
         return noiseChance
 
+
 class TrialTypeField(StimSpecDataField):
 
     def __init__(self, conn: Connection):
-        super().__init__(conn, "TrialType")
+        super().__init__(conn, name="TrialType")
 
     def get(self, when: When):
         stim_spec_data = StimSpecDataField.get(self, when)
--- EStimShapeAnalysis/src/analysis/nafc/psychometric_curves.py
+++ EStimShapeAnalysis/src/analysis/nafc/psychometric_curves.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 from analysis.nafc.nafc_database_fields import IsCorrectField, NoiseChanceField
 from clat.compile.trial.trial_collector import TrialCollector
 from clat.compile.trial.trial_field import FieldList
+from clat.compile.trial.cached_fields import CachedFieldList
 from clat.util import time_util
 from clat.util.connection import Connection
 from clat.util.time_util import When
@@ -14,14 +15,15 @@ def collect_choice_trials(conn: Connection, when: When = time_util.all()) -> lis
     trial_collector = TrialCollector(conn, when)
     return trial_collector.collect_choice_trials()
 
+
 def main():
     conn = Connection("allen_estimshape_train_231211")
     trial_tstamps = collect_choice_trials(conn, time_util.on_date_and_time(2024,
                                                                            1, 3,
-                                                                           start_time="16:49:00", #"16:49:00"
+                                                                           start_time="16:49:00",  # "16:49:00"
                                                                            end_time=None))
 
-    fields = FieldList()
+    fields = CachedFieldList()
     fields.append(IsCorrectField(conn))
     fields.append(NoiseChanceField(conn))
 
@@ -30,12 +32,13 @@ def main():
 
     plot_percent_correct(data)
 
+
 def plot_percent_correct(df):
     # Group by 'NoiseChance' and calculate the percentage of 'Correct' in 'IsCorrect'
     percent_correct = df.groupby('NoiseChance')['IsCorrect'].apply(lambda x: (x == 'Correct').sum() / len(x) * 100)
 
     # Sort the percent_correct Series in descending order of 'NoiseChance'
-    percent_correct = percent_correct.sort_index(ascending=False)
+    percent_correct = percent_correct.sort_index(ascending=True)
 
     # Plotting as a line graph
     plt.figure(figsize=(10, 6))
@@ -46,8 +49,6 @@ def plot_percent_correct(df):
     plt.grid(True)
     plt.gca().invert_xaxis()  # Invert x-axis to have higher NoiseChance first
     plt.show()
-
-
 
 
 if __name__ == '__main__':
