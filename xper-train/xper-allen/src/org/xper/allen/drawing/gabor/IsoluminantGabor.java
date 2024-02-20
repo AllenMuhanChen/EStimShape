000caea80cca33101e07571f50e9063a2d2e38ff
Allen Chen <allenmuhanchen@gmail.com>
2024-02-20 15:51:35 -0500

    Working Isochromatic and Isoluminant Test Gabors

---
+++ xper-train/xper-allen/src/org/xper/allen/drawing/gabor/IsoluminantGabor.java
@@ -0,0 +1,62 @@
+package org.xper.allen.drawing.gabor;
+
+import org.xper.drawing.RGBColor;
+
+import java.awt.*;
+
+public class IsoluminantGabor extends Gabor {
+
+    RGBColor color1;
+    RGBColor color2;
+    boolean modRedGreen;
+    boolean modBlueYellow;
+    ColourConverter.WhitePoint whitePoint = ColourConverter.WhitePoint.D65;
+
+    public IsoluminantGabor(RGBColor color1, RGBColor color2, boolean modRedGreen, boolean modBlueYellow) {
+        this.color1 = color1;
+        this.color2 = color2;
+        this.modRedGreen = modRedGreen;
+        this.modBlueYellow = modBlueYellow;
+    }
+
+    @Override
+    protected float[] modulateColor(float modFactor) {
+        // Ensure modFactor is within 0 and 1
+        modFactor = Math.max(0, Math.min(modFactor, 1));
+
+        // Convert both colors to Lab
+        double[] lab1 = ColourConverter.getLab(new Color(color1.getRed(), color1.getGreen(), color1.getBlue()), whitePoint);
+        double[] lab2 = ColourConverter.getLab(new Color(color2.getRed(), color2.getGreen(), color2.getBlue()), whitePoint);
+
+        // Interpolate the a* and b* components between the two colors based on modFactor
+        // Maintain constant L* (luminance) from the first color (or average them if you prefer)
+        double L = lab1[0]; // or (lab1[0] + lab2[0]) / 2 for average luminance
+        double a;
+        double b;
+        if (modRedGreen) {
+            a = interpolate(lab1[1], lab2[1], modFactor);
+        }
+        else {
+            a = lab1[1];
+        }
+        if (modBlueYellow)
+            b = interpolate(lab1[2], lab2[2], modFactor);
+        else
+            b = lab1[2];
+
+        // Convert the interpolated Lab color back to RGB
+        double[] modulatedRGB = ColourConverter.labToRGB(L, a, b, whitePoint);
+
+        // Return as an array for OpenGL
+        float[] rgb = new float[3];
+        rgb[0] = (float) (modulatedRGB[0]);
+        rgb[1] = (float) (modulatedRGB[1]);
+        rgb[2] = (float) (modulatedRGB[2]);
+
+        return rgb;
+    }
+
+    private double interpolate(double value1, double value2, float factor) {
+        return value1 + (value2 - value1) * factor;
+    }
+}
--- xper-train/xper-allen/test/unit-tests/org/xper/allen/drawing/gabor/IsochromaticGaborTest.java
+++ xper-train/xper-allen/test/unit-tests/org/xper/allen/drawing/gabor/IsochromaticGaborTest.java
@@ -2,6 +2,7 @@ package org.xper.allen.drawing.gabor;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.lwjgl.opengl.GL11;
 import org.xper.alden.drawing.drawables.Drawable;
 import org.xper.drawing.RGBColor;
 import org.xper.drawing.TestDrawingWindow;
@@ -24,7 +25,7 @@ public class IsochromaticGaborTest {
     }
 
     @Test
-    public void testDraw() {
+    public void testIsochromatic() {
         GratingSpec spec = new GratingSpec();
         spec.setOrientation(0);
         spec.setPhase(0);
@@ -41,11 +42,42 @@ public class IsochromaticGaborTest {
         window.draw(new Drawable() {
             @Override
             public void draw() {
+                GL11.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+                GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
                 IsochromaticGabor.initGL(width, height);
                 gabor.draw(null);
             }
         });
 
         ThreadUtil.sleep(10000);
+    }
+
+    @Test
+    public void testIsoluminant() {
+        GratingSpec spec = new GratingSpec();
+        spec.setOrientation(0);
+        spec.setPhase(0);
+        spec.setFrequency(10);
+        spec.setXCenter(0);
+        spec.setYCenter(0);
+        spec.setSize(50);
+        spec.setAnimation(false);
+
+        IsoluminantGabor gabor = new IsoluminantGabor(new RGBColor(0, 0.5f, 0.5f), new RGBColor(0.5f, 0.5f, 0),
+                true, true);
+        gabor.setSpec(spec);
+
+        window.draw(new Drawable() {
+            @Override
+            public void draw() {
+                GL11.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+                GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+                IsoluminantGabor.initGL(width, height);
+                gabor.draw(null);
+            }
+        });
+
+        ThreadUtil.sleep(100000);
+
     }
 }
