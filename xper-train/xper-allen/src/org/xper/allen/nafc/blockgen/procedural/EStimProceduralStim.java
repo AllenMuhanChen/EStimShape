b412ae7cf46dd4eddfc9bd199471b783664788ad
Allen Chen <allenmuhanchen@gmail.com>
2024-05-30 09:07:35 -0400

    modified EStimProceduralStim to have choice images be presented so that mSticks are around same size as sample. added mouse and unmouse shellScripts.

--- xper-train/xper-allen/src/org/xper/allen/nafc/blockgen/procedural/EStimProceduralStim.java
+++ xper-train/xper-allen/src/org/xper/allen/nafc/blockgen/procedural/EStimProceduralStim.java
@@ -5,12 +5,16 @@ import org.xper.allen.drawing.composition.experiment.EStimShapeProceduralMatchSt
 import org.xper.allen.drawing.composition.experiment.ProceduralMatchStick;
 import org.xper.allen.nafc.blockgen.psychometric.NAFCStimSpecWriter;
 import org.xper.allen.nafc.experiment.RewardPolicy;
+import org.xper.allen.nafc.vo.MStickStimObjData;
 import org.xper.allen.pga.RFStrategy;
 import org.xper.allen.pga.RFUtils;
 import org.xper.allen.pga.ReceptiveFieldSource;
+import org.xper.allen.specs.NoisyPngSpec;
 import org.xper.allen.util.AllenDbUtil;
+import org.xper.rfplot.drawing.png.ImageDimensions;
 
 import javax.vecmath.Point3d;
+import java.awt.*;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -118,5 +122,83 @@ public class EStimProceduralStim extends ProceduralStim{
                 stimObjIds, RewardPolicy.LIST, rewardList);
 
         stimSpecWriter.writeStimSpec();
+    }
+
+    /**
+     * Different from super class in that there is separate image dimensions for sample and choices.
+     * For the sample, we use the size specified in SystemVars
+     * For the choices, we use size specified by RFUtils.calculateMStickMaxSizeDegrees
+     * so that the size of choices are going to be approximately similar to the sample.
+     */
+    @Override
+    protected void writeStimObjDataSpecs() {
+        double imageSizeSample = generator.getImageDimensionsDegrees();
+        ImageDimensions dimensionsSample = new ImageDimensions(imageSizeSample, imageSizeSample);
+
+        double imageSizeChoices = RFUtils.calculateMStickMaxSizeDegrees(RFStrategy.PARTIALLY_INSIDE, rfSource);
+        ImageDimensions dimensionsChoices = new ImageDimensions(imageSizeChoices, imageSizeChoices);
+
+        //Sample
+        double xCenter = coords.getSample().getX();
+        double yCenter = coords.getSample().getY();
+        String path = experimentPngPaths.getSample();
+        String noiseMapPath = experimentNoiseMapPath;
+        Color color = parameters.color;
+        double numNoiseFrames = parameters.noiseRate;
+        NoisyPngSpec sampleSpec = new NoisyPngSpec(
+                xCenter, yCenter,
+                dimensionsSample,
+                path,
+                noiseMapPath,
+                color,
+                numNoiseFrames,
+                parameters.noiseChance);
+        MStickStimObjData sampleMStickObjData = new MStickStimObjData("sample", mStickSpecs.getSample());
+        AllenDbUtil dbUtil = (AllenDbUtil) generator.getDbUtil();
+        dbUtil.writeStimObjData(stimObjIds.getSample(), sampleSpec.toXml(), sampleMStickObjData.toXml());
+
+        //Match
+        xCenter = coords.getMatch().getX();
+        yCenter = coords.getMatch().getY();
+        path = experimentPngPaths.getMatch();
+        noiseMapPath = "";
+        NoisyPngSpec matchSpec = new NoisyPngSpec(
+                xCenter, yCenter,
+                dimensionsChoices,
+                path,
+                noiseMapPath,
+                color);
+        MStickStimObjData matchMStickObjData = new MStickStimObjData("match", mStickSpecs.getMatch());
+        dbUtil.writeStimObjData(stimObjIds.getMatch(), matchSpec.toXml(), matchMStickObjData.toXml());
+
+        //Procedural Distractors
+        for (int i = 0; i < numProceduralDistractors; i++) {
+            xCenter = coords.proceduralDistractors.get(i).getX();
+            yCenter = coords.proceduralDistractors.get(i).getY();
+            path = experimentPngPaths.proceduralDistractors.get(i);
+            NoisyPngSpec proceduralDistractorSpec = new NoisyPngSpec(
+                    xCenter, yCenter,
+                    dimensionsChoices,
+                    path,
+                    noiseMapPath,
+                    color);
+            MStickStimObjData proceduralDistractorMStickObjData = new MStickStimObjData("procedural", mStickSpecs.proceduralDistractors.get(i));
+            dbUtil.writeStimObjData(stimObjIds.proceduralDistractors.get(i), proceduralDistractorSpec.toXml(), proceduralDistractorMStickObjData.toXml());
+        }
+
+        //Rand Distractors
+        for (int i = 0; i < numRandDistractors; i++) {
+            xCenter = coords.randDistractors.get(i).getX();
+            yCenter = coords.randDistractors.get(i).getY();
+            path = experimentPngPaths.randDistractors.get(i);
+            NoisyPngSpec randDistractorSpec = new NoisyPngSpec(
+                    xCenter, yCenter,
+                    dimensionsChoices,
+                    path,
+                    noiseMapPath,
+                    color);
+            MStickStimObjData randDistractorMStickObjData = new MStickStimObjData("rand", mStickSpecs.randDistractors.get(i));
+            dbUtil.writeStimObjData(stimObjIds.randDistractors.get(i), randDistractorSpec.toXml(), randDistractorMStickObjData.toXml());
+        }
     }
 }
