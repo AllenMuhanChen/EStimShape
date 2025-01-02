e7343adc49c86e7b7712c66d09daf4e2102bc4ed
Allen Chen <allenmuhanchen@gmail.com>
2025-01-02 17:47:33 -0500

    IsoGaborTrialGenerator now reads location, size and orientation from database

--- xper-train/xper-allen/src/org/xper/allen/isoluminant/IsoGaborTrialGenerator.java
+++ xper-train/xper-allen/src/org/xper/allen/isoluminant/IsoGaborTrialGenerator.java
@@ -1,43 +1,69 @@
 package org.xper.allen.isoluminant;
 
 import org.springframework.config.java.context.JavaConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.xper.allen.Stim;
 import org.xper.allen.nafc.blockgen.AbstractTrialGenerator;
+import org.xper.rfplot.RFInfo;
 import org.xper.rfplot.drawing.GaborSpec;
 import org.xper.rfplot.drawing.gabor.IsoGaborSpec;
 import org.xper.util.FileUtil;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class IsoGaborTrialGenerator extends AbstractTrialGenerator<Stim> {
-
     private final int numRepeats = 1;
     private GaborSpec gaborSpec;
     public static final List<Double> frequencies = Arrays.asList(0.5, 1.0, 2.0, 4.0);
     public static final List<Double> mixedPhases = Arrays.asList(0.0, 0.25, 0.5, 0.75);
-//    public static final List<Double> frequencies = Arrays.asList(4.0);
 
     public static void main(String[] args) {
-        JavaConfigApplicationContext context = new JavaConfigApplicationContext(FileUtil.loadConfigClass("experiment.config_class"), IsoGaborConfig.class);
+        JavaConfigApplicationContext context = new JavaConfigApplicationContext(
+                FileUtil.loadConfigClass("experiment.config_class"),
+                IsoGaborConfig.class
+        );
         IsoGaborTrialGenerator gen = context.getBean(IsoGaborTrialGenerator.class);
-
         gen.generate();
+    }
+
+
+    @Override
+    protected void writeTrials() {
+        for (Stim stim : getStims()) {
+            stim.writeStim();
+            Long stimId = stim.getStimId();
+            for (int i = 0; i < numRepeats; i++) {
+                long taskId = getGlobalTimeUtil().currentTimeMicros();
+                dbUtil.writeTaskToDo(taskId, stimId, -1, genId);
+            }
+        }
     }
 
     @Override
     protected void addTrials() {
-        int size = 4;
-        int orientation = 45;
-        int xCenter = 5;
-        int yCenter = 5;
+        // Get database values
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dbUtil.getDataSource());
+
+        // Get RF Info
+        RFInfo rfInfo = getRFInfo(jdbcTemplate);
+
+        // Get StimSpec
+        GaborSpec stimSpec = getStimSpec(jdbcTemplate);
+
+        // Set up GaborSpec with database values
         gaborSpec = new GaborSpec();
-        gaborSpec.setOrientation(orientation);
+        gaborSpec.setXCenter(rfInfo.getCenter().getX());
+        gaborSpec.setYCenter(rfInfo.getCenter().getY());
+        gaborSpec.setSize(rfInfo.getRadius()*1.5);
+        gaborSpec.setOrientation(stimSpec.getOrientation());
         gaborSpec.setPhase(0);
-        gaborSpec.setXCenter(xCenter);
-        gaborSpec.setYCenter(yCenter);
-        gaborSpec.setSize(size);
         gaborSpec.setAnimation(false);
 
         // Pure Gabors
@@ -48,7 +74,7 @@ public class IsoGaborTrialGenerator extends AbstractTrialGenerator<Stim> {
         }
 
         // Mixed Gabors - frequencies
-        for (Double frequencyChromatic : frequencies){
+        for (Double frequencyChromatic : frequencies) {
             GaborSpec chromaticSpec = new GaborSpec(gaborSpec);
             chromaticSpec.setFrequency(frequencyChromatic);
             for (Double frequencyLuminance : frequencies) {
@@ -65,11 +91,11 @@ public class IsoGaborTrialGenerator extends AbstractTrialGenerator<Stim> {
             }
         }
 
-        //Mixed Gabors - Phases
-        for (double frequency: frequencies){
+        // Mixed Gabors - Phases
+        for (double frequency : frequencies) {
             GaborSpec chromaticSpec = new GaborSpec(gaborSpec);
             chromaticSpec.setFrequency(frequency);
-            for (double phase : mixedPhases){
+            for (double phase : mixedPhases) {
                 double phaseLuminance = phase * frequency;
 
                 GaborSpec luminanceSpec = new GaborSpec(gaborSpec);
@@ -120,15 +146,34 @@ public class IsoGaborTrialGenerator extends AbstractTrialGenerator<Stim> {
     }
 
 
-    @Override
-    protected void writeTrials(){
-        for (Stim stim : getStims()) {
-            stim.writeStim();
-            Long stimId = stim.getStimId();
-            for (int i = 0; i < numRepeats; i++) {
-                long taskId = getGlobalTimeUtil().currentTimeMicros();
-                dbUtil.writeTaskToDo(taskId, stimId, -1, genId);
-            }
-        }
+    private long getLatestTimestamp(JdbcTemplate jdbcTemplate, String table) {
+        return (long) jdbcTemplate.queryForObject(
+                "SELECT MAX(tstamp) FROM " + table + " WHERE channel = 'SUPRA-000'",
+                Long.class
+        );
     }
+
+    private RFInfo getRFInfo(JdbcTemplate jdbcTemplate) {
+        long latestTimestamp = getLatestTimestamp(jdbcTemplate, "RFInfo");
+        String rfInfoXml = (String) jdbcTemplate.queryForObject(
+                "SELECT info FROM RFInfo WHERE tstamp = ? AND channel = 'SUPRA-000'",
+                new Object[]{latestTimestamp},
+                String.class
+        );
+        return RFInfo.fromXml(rfInfoXml);
+    }
+
+    private GaborSpec getStimSpec(JdbcTemplate jdbcTemplate) {
+        long latestTimestamp = getLatestTimestamp(jdbcTemplate, "RFObjectData");
+        String stimSpecXml = (String) jdbcTemplate.queryForObject(
+                "SELECT data FROM RFObjectData WHERE tstamp = ? AND channel = 'SUPRA-000' AND object = 'org.xper.rfplot.drawing.gabor.Gabor'",
+                new Object[]{latestTimestamp},
+                String.class
+        );
+        return GaborSpec.fromXml(stimSpecXml);
+    }
+
+
+
+
 }
