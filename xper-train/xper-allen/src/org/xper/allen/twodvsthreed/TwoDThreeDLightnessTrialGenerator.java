4ef178b6b8c3c232e23a22aa91a99f5df9a7cc50
Allen Chen <allenmuhanchen@gmail.com>
2025-04-06 17:21:23 -0400

    fixed issue with wrong lightnesses being tested in Lightness test

--- xper-train/xper-allen/src/org/xper/allen/twodvsthreed/TwoDThreeDLightnessTrialGenerator.java
+++ xper-train/xper-allen/src/org/xper/allen/twodvsthreed/TwoDThreeDLightnessTrialGenerator.java
@@ -90,14 +90,13 @@ public class TwoDThreeDLightnessTrialGenerator extends TwoDVsThreeDTrialGenerato
 
         // GENERATE TRIALS
         for (Long gaStimId : stimIdsToTest) {
-            List<Double> contrastsToTest = fetchContrastsToTest(gaStimId);
-            for (Double contrast : contrastsToTest) {
+            for (Double lightness : LIGHTNESSES_TO_TEST) {
                 for (String textureType : textureTypesToTest) {
                     // Take the original color and apply the contrast we want to test to it, while keeping the
                     // actual contrast variable the same. This is because contrast only applies to the drawing of the
                     // 2D stimuli, not the 3D ones.
                     RGBColor color = fetchColorForStimId(gaStimId);
-                    color = new RGBColor(color.getRed() * contrast, color.getGreen() * contrast, color.getBlue() * contrast);
+                    color = new RGBColor(color.getRed() * lightness, color.getGreen() * lightness, color.getBlue() * lightness);
                     TwoDVsThreeDStim stim = new TwoDVsThreeDStim(this, gaStimId, textureType, color, 1.0);
                     stims.add(stim);
                 }
@@ -131,11 +130,8 @@ public class TwoDThreeDLightnessTrialGenerator extends TwoDVsThreeDTrialGenerato
         }
     }
 
-    private List<Double> fetchContrastsToTest(Long stimId) {
-        List<Double> contrastsToTest = new ArrayList<>();
-        contrastsToTest.add(0.4);
-        contrastsToTest.add(1.0);
-        return contrastsToTest;
+    private List<Double> fetchContrastsToTest() {
+        return LIGHTNESSES_TO_TEST;
     }
 
     private List<RGBColor> fetchColorsToTest(Long stimId) {
